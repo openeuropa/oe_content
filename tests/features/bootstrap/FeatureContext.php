aa6a4a37fcd934adb3593878d82a92075d0f4606
voidtek <vdacosta@voidtek.com>
2018-08-02 13:34:56 +0000

    OPENEUROPA-726: Initialize oe_content module.

---
+++ tests/features/bootstrap/FeatureContext.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types = 1);
+
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+
+/**
+ * Defines step definitions that are generally useful in this project.
+ */
+class FeatureContext extends RawDrupalContext {
+}
