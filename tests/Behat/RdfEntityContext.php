23d855436e7d5bca048ad1e228375d464a445af8
upchuk <danny@webomelette.com>
2018-08-14 11:57:28 +0200

    OPENEUROPA-1111: Provenance access rules + test.

--- tests/Behat/RdfEntityContext.php
+++ tests/Behat/RdfEntityContext.php
@@ -6,15 +6,19 @@ namespace Drupal\Tests\oe_content\Behat;
 
 use Behat\Behat\Hook\Scope\AfterScenarioScope;
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
-use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Drupal\DrupalExtension\Context\ConfigContext;
+use Drupal\rdf_entity\RdfInterface;
 
 /**
  * Defines step definitions specifically for testing the RDF entities.
  *
  * This is needed due to issues with the Kernel testing of the RDF entity module
  * in Drupal 8.6.
+ *
+ * We are extending ConfigContext to override the setConfig() method until
+ * issue https://github.com/jhedstrom/drupalextension/issues/498 is fixed.
  */
-class RdfEntityContext extends RawDrupalContext {
+class RdfEntityContext extends ConfigContext {
 
   /**
    * An array of RDF entities we need to keep track to delete.
@@ -22,26 +26,6 @@ class RdfEntityContext extends RawDrupalContext {
    * @var \Drupal\rdf_entity\RdfInterface[]
    */
   protected $rdfEntities = [];
-
-  /**
-   * The Configuration context.
-   *
-   * @var \Drupal\DrupalExtension\Context\ConfigContext
-   */
-  protected $configContext;
-
-  /**
-   * Before scenario hook get the ConfigContext.
-   *
-   * @param \Behat\Behat\Hook\Scope\BeforeScenarioScope $scope
-   *   The Hook scope.
-   *
-   * @BeforeScenario
-   */
-  public function gatherConfigContext(BeforeScenarioScope $scope): void {
-    $environment = $scope->getEnvironment();
-    $this->configContext = $environment->getContext('Drupal\DrupalExtension\Context\ConfigContext');
-  }
 
   /**
    * After scenario hook to delete the RDF entities.
@@ -70,16 +54,17 @@ class RdfEntityContext extends RawDrupalContext {
    * @BeforeScenario @rdf-provenance
    */
   public function setDefaultProvenance(BeforeScenarioScope $scope): void {
-    $this->configContext->setConfig('oe_content.settings', 'provenance_uri', 'http://example.com');
+    $this->setConfig('oe_content.settings', 'provenance_uri', 'http://example.com');
   }
 
   /**
    * Configures the Provenance URI.
    *
    * @Given the site Provenance URI is set to :uri
+   * @And the site Provenance URI is set to :uri
    */
   public function theSiteProvenanceUriIsSetTo($uri): void {
-    $this->configContext->setConfig('oe_content.settings', 'provenance_uri', $uri);
+    $this->setConfig('oe_content.settings', 'provenance_uri', $uri);
   }
 
   /**
@@ -90,7 +75,7 @@ class RdfEntityContext extends RawDrupalContext {
    *
    * @Given I am logged in with a user that can create and view :bundle RDF entities
    */
-  public function iAmLoggedInWithUserThatCanCreateAndViewRdfEntityTypes($bundle) {
+  public function iAmLoggedInWithUserThatCanCreateAndViewRdfEntityTypes($bundle): void {
     /** @var \Drupal\rdf_entity\RdfEntityTypeInterface[] $types */
     $types = \Drupal::entityTypeManager()->getStorage('rdf_type')->loadMultiple();
     $permission_map = [];
@@ -143,14 +128,8 @@ class RdfEntityContext extends RawDrupalContext {
    * @Then the Provenance URI of the RDF entity with the name :name should be :provenance_uri
    */
   public function theProvenanceUriOfTheRdfEntityWithTheNameShouldBe($name, $provenance_uri): void {
-    $entities = \Drupal::entityTypeManager()->getStorage('rdf_entity')->loadByProperties(['label' => $name]);
-    if (!$entities) {
-      throw new \Exception('The RDF entity could not be found.');
-    }
-
-    /** @var \Drupal\rdf_entity\RdfInterface $rdf */
-    $rdf = reset($entities);
-    if ($rdf->get('provenance_uri')->value !== $provenance_uri) {
+    $entity = $this->loadRdfEntityByName($name);
+    if ($entity->get('provenance_uri')->value !== $provenance_uri) {
       throw new \Exception('The RDF entity did not get created with the proper Provenance URI.');
     }
   }
@@ -161,14 +140,34 @@ class RdfEntityContext extends RawDrupalContext {
    * @Then /^I delete the RDF entity with the name "([^"]*)"$/
    */
   public function iDeleteTheRdfEntityWithTheName($arg1): void {
-    $entities = \Drupal::entityTypeManager()->getStorage('rdf_entity')->loadByProperties(['label' => $arg1]);
-    if ($entities) {
-      $entity = reset($entities);
-      $entity->delete();
-      return;
-    }
+    $entity = $this->loadRdfEntityByName($arg1);
+    $entity->delete();
+  }
 
-    throw new \Exception('RDF entity was not found to be deleted.');
+  /**
+   * Asserts that the current user has access to a given RDF entity.
+   *
+   * @Then I should have :operation access to the RDF entity with the name :name
+   */
+  public function iShouldHaveAccessToTheRdfEntityWithTheName($operation, $name): void {
+    $entity = $this->loadRdfEntityByName($name);
+    if (!$entity->access($operation)) {
+      throw new \Exception('The current user does not have access for this operation and they should.');
+    }
+  }
+
+  /**
+   * Asserts that the current user does not have access to a given RDF entity.
+   *
+   * @Then I should not have :operation access to the RDF entity with the name :name
+   */
+  public function iShouldNotHaveAccessToTheRdfEntityWithTheName($operation, $name): void {
+    $entity = $this->loadRdfEntityByName($name);
+    var_dump('entiy prov: ' . $entity->get('provenance_uri')->value);
+    var_dump('site prov: ' . \Drupal::config('oe_content.settings')->get('provenance_uri'));
+    if ($entity->access($operation)) {
+      throw new \Exception('The current user has access for this operation and they should not..');
+    }
   }
 
   /**
@@ -200,6 +199,44 @@ class RdfEntityContext extends RawDrupalContext {
     $entity = \Drupal::entityTypeManager()->getStorage('rdf_entity')->create($values);
     $entity->save();
     $this->rdfEntities[] = $entity;
+  }
+
+  /**
+   * Loads an RDF entity by name.
+   *
+   * @param string $name
+   *   The name of the entity.
+   *
+   * @return \Drupal\rdf_entity\RdfInterface
+   *   The RDF entity instance.
+   */
+  protected function loadRdfEntityByName($name): RdfInterface {
+    $entities = \Drupal::entityTypeManager()->getStorage('rdf_entity')->loadByProperties(['label' => $name]);
+
+    if (!$entities) {
+      throw new \Exception('RDF entity not found.');
+    }
+
+    return reset($entities);
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * To be removed when https://github.com/jhedstrom/drupalextension/issues/498
+   * gets fixed.
+   */
+  public function setConfig($name, $key, $value) {
+    $backup = $this->getDriver()->configGet($name, $key);
+    $this->getDriver()->configSet($name, $key, $value);
+    if (!array_key_exists($name, $this->config)) {
+      $this->config[$name][$key] = $backup;
+      return;
+    }
+
+    if (!array_key_exists($key, $this->config[$name])) {
+      $this->config[$name][$key] = $backup;
+    }
   }
 
 }
--- tests/features/provenance_uri.feature
+++ tests/features/provenance_uri.feature
@@ -9,3 +9,15 @@ Feature: Provenance feature
     Given the site Provenance URI is set to "http://example-provenance.com"
     And I create an "Announcement" RDF entity with the name "Test Provenance"
     Then the Provenance URI of the RDF entity with the name "Test Provenance" should be "http://example-provenance.com"
+
+  @rdf-test
+  Scenario: Access to manage RDF entities needs to take the provenance URI into account.
+    Given I am logged in with a user that can create and view "Announcement" RDF entities
+    And the site Provenance URI is set to "http://example-provenance.com"
+    And I create an "Announcement" RDF entity with the name "Test Provenance Access One"
+    And the site Provenance URI is set to "http://example-provenance-another-site.com"
+    And I create an "Announcement" RDF entity with the name "Test Provenance Access Two"
+    Then I should not have "edit" access to the RDF entity with the name "Test Provenance Access One"
+    And I should not have "delete" access to the RDF entity with the name "Test Provenance Access One"
+    And I should have "edit" access to the RDF entity with the name "Test Provenance Access Two"
+    And I should have "edit" access to the RDF entity with the name "Test Provenance Access Two"
