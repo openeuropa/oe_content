52ed2a771c5209e332c9278bd42097f68e1ae95d
Hernani Borges de Freitas <hernani@Hernanis-MacBook-Air.local>
2019-03-25 12:20:45 +0000

    OPENEUROPA-1723: Small fixes on the implementation and test.

--- tests/Kernel/ShortTitleTokenReplaceTest.php
+++ tests/Kernel/ShortTitleTokenReplaceTest.php
@@ -27,15 +27,11 @@ class ShortTitleTokenReplaceTest extends RdfKernelTestBase {
    */
   public static $modules = [
     'content_translation',
-    'datetime',
     'field',
     'language',
     'link',
     'node',
     'oe_content',
-    'rdf_draft',
-    'rdf_entity',
-    'rdf_entity_test',
     'rdf_skos',
     'system',
     'text',
@@ -58,16 +54,16 @@ class ShortTitleTokenReplaceTest extends RdfKernelTestBase {
     module_load_include('install', 'oe_content');
     oe_content_install();
 
-    // Copied from @TokenReplaceKernelTestBase.
+    // Copied from \Drupal\Tests\system\Kernel\Token\TokenReplaceKernelTestBase.
     $this->installConfig(['system']);
     \Drupal::service('router.builder')->rebuild();
-
     $this->interfaceLanguage = \Drupal::languageManager()->getCurrentLanguage();
     $this->tokenService = \Drupal::token();
-
+    $this->entityTypeManager = \Drupal::entityTypeManager()->getStorage('node');
     // Create french language.
     ConfigurableLanguage::createFromLangcode('fr')->save();
 
+    // Create node type for the test.
     $node_type = NodeType::create(['type' => 'oe_content_token_test', 'name' => 'OE Content token test']);
     $node_type->save();
 
@@ -78,30 +74,37 @@ class ShortTitleTokenReplaceTest extends RdfKernelTestBase {
    */
   public function testShortTitleTokenReplacement() {
 
-    // Create a user and a node.
+    // Create a user and a node with short title.
     $account = $this->createUser();
     /* @var $node \Drupal\node\NodeInterface */
-    $node = Node::create([
+    $node_short_titled = Node::create([
       'type' => 'oe_content_token_test',
       'tnid' => 0,
       'uid' => $account->id(),
       'title' => 'I am a title',
       'oe_content_short_title' => 'I am a short title',
     ]);
+    $node_short_titled->save();
 
-    $node->save();
+    // Creates a node without short title.
+    $node_titled = Node::create([
+      'type' => 'oe_content_token_test',
+      'tnid' => 0,
+      'uid' => $account->id(),
+      'title' => 'I am a title',
+      'oe_content_short_title' => '',
+    ]);
+    $node_titled->save();
 
-    $entity_type_manager = \Drupal::entityTypeManager()->getStorage('node');
-    $entity_type_manager->resetCache();
+    $this->entityTypeManager->resetCache();
     /** @var \Drupal\node\NodeInterface $node */
-    $node = $entity_type_manager->load($node->id());
+    $node = $this->entityTypeManager->load($node_short_titled->id());
 
     // Generate and test tokens.
     $tests = [];
-    $tests['[node:short-title-fallback]'] = $node->get('oe_content_short_title')->value ?? $node->label();
-
-    $base_metadata = BubbleableMetadata::createFromObject($node);
-
+    $tests['[node:short-title-fallback]'] = $node_short_titled->get('oe_content_short_title')->value;
+    $base_metadata = BubbleableMetadata::createFromObject($node_short_titled);
+    $bubbleable_metadata = new BubbleableMetadata();
     $metadata_tests = [];
     $metadata_tests['[node:short-title-fallback]'] = $base_metadata;
 
@@ -109,27 +112,36 @@ class ShortTitleTokenReplaceTest extends RdfKernelTestBase {
     $this->assertFalse(in_array(0, array_map('strlen', $tests)), 'No empty tokens generated.');
 
     foreach ($tests as $input => $expected) {
-      $bubbleable_metadata = new BubbleableMetadata();
-      $output = $this->tokenService->replace($input, ['node' => $node], ['langcode' => $this->interfaceLanguage->getId()], $bubbleable_metadata);
+
+      $output = $this->tokenService->replace($input, ['node' => $node_short_titled], ['langcode' => $this->interfaceLanguage->getId()], $bubbleable_metadata);
       $this->assertEqual($output, $expected, format_string('Node token %token replaced.', ['%token' => $input]));
       $this->assertEqual($bubbleable_metadata, $metadata_tests[$input]);
     }
 
-    // Adds a french translation.
-    $node->addTranslation('fr', [
+    // Adds a french translation and tests if value from source translation is
+    // still used.
+    $node_short_titled->addTranslation('fr', [
       'uid' => $account->id(),
       'title' => 'Je suis un titre',
-      'oe_content_short_title' => 'Je suis un petite titre',
+      'oe_content_short_title' => 'Je suis un petit titre',
     ]);
 
-    $node->save();
+    $node_short_titled->save();
 
-    $tests = [];
-    $tests['[node:short-title-fallback]'] = $node->getUntranslated()->get('oe_content_short_title')->value ?? $node->label();
-
+    $tests['[node:short-title-fallback]'] = $node_short_titled->getUntranslated()->get('oe_content_short_title')->value;
     foreach ($tests as $input => $expected) {
-      $bubbleable_metadata = new BubbleableMetadata();
-      $output = $this->tokenService->replace($input, ['node' => $node], ['langcode' => $this->interfaceLanguage->getId()], $bubbleable_metadata);
+      $output = $this->tokenService->replace($input, ['node' => $node_short_titled], ['langcode' => $this->interfaceLanguage->getId()], $bubbleable_metadata);
+      $this->assertEqual($output, $expected, format_string('Node token %token replaced.', ['%token' => $input]));
+      $this->assertEqual($bubbleable_metadata, $metadata_tests[$input]);
+    }
+
+    // Tests the token with a node without short title field value.
+    $tests['[node:short-title-fallback]'] = $node_titled->getUntranslated()->getTitle();
+    $bubbleable_metadata = new BubbleableMetadata();
+    $base_metadata = BubbleableMetadata::createFromObject($node_titled);
+    $metadata_tests['[node:short-title-fallback]'] = $base_metadata;
+    foreach ($tests as $input => $expected) {
+      $output = $this->tokenService->replace($input, ['node' => $node_titled], ['langcode' => $this->interfaceLanguage->getId()], $bubbleable_metadata);
       $this->assertEqual($output, $expected, format_string('Node token %token replaced.', ['%token' => $input]));
       $this->assertEqual($bubbleable_metadata, $metadata_tests[$input]);
     }
