28cf61ffeaa6be9ba3e0a780ca31d68e658137a5
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2019-03-25 16:23:46 +0100

    OPENEUROPA-1737: Update controller for canonical urls.

--- modules/oe_content_canonical/src/Controller/CanonicalUrlController.php
+++ modules/oe_content_canonical/src/Controller/CanonicalUrlController.php
@@ -6,29 +6,57 @@ namespace Drupal\oe_content_canonical\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\oe_content_canonical\ContentUuidResolverInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
- * Returns responses for Node routes.
+ * Returns response for redirect to node (or other entity types) aliases.
  */
 class CanonicalUrlController extends ControllerBase implements ContainerInjectionInterface {
 
-    public function __construct() {
+  /**
+   * The Content UUID transformer to alias/system path.
+   *
+   * @var \Drupal\oe_content_canonical\ContentUuidResolverInterface
+   */
+  protected $contentUuidResolver;
 
+  /**
+   * CanonicalUrlController constructor.
+   *
+   * @param \Drupal\oe_content_canonical\ContentUuidResolverInterface $uuid_resolver
+   *   The service for transforming uuid to alias/system path.
+   */
+  public function __construct(ContentUuidResolverInterface $uuid_resolver) {
+    $this->contentUuidResolver = $uuid_resolver;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('oe_content_canonical.resolver')
+    );
+  }
+
+  /**
+   * The controller callback method for handling redirect of canonical urls.
+   *
+   * @param string $uuid
+   *   The UUID of content for redirect.
+   *
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse
+   *   A redirect response to actual alias or system path.
+   */
+  public function index(string $uuid): RedirectResponse {
+    $this->contentUuidResolver->setCacheKey($uuid);
+    if ($alias = $this->contentUuidResolver->getAliasByUuid($uuid)) {
+      return new RedirectResponse($alias, 302);
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function create(ContainerInterface $container) {
-        return new static(
-        );
-    }
-
-    public function index(string $uuid): RedirectResponse {
-        return $this->redirect('entity.node.canonical', ['node' => 333]);
-    }
-
+    return $this->redirect('system.404');
+  }
 
 }
