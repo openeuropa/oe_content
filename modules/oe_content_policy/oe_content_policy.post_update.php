dc3519bf4faffc9b057846cfec63bc0bdd052661
nagyad <joevagyok@gmail.com>
2019-07-25 09:48:42 +0200

    OPENEUROPA-2047: Check for field config in upgrade.

--- modules/oe_content_policy/oe_content_policy.post_update.php
+++ modules/oe_content_policy/oe_content_policy.post_update.php
@@ -13,21 +13,28 @@ use Drupal\field\Entity\FieldStorageConfig;
 /**
  * Add timeline field to policy content type.
  */
-function oe_content_policy_post_update_00001_timeline_field(array &$sandbox): void {
-  FieldStorageConfig::create([
-    'field_name' => 'oe_timeline',
-    'entity_type' => 'node',
-    'type' => 'timeline_field',
-    'cardinality' => -1,
-    'entity_types' => ['node'],
-  ])->save();
+function oe_content_policy_post_update_00003_timeline_field(array &$sandbox): void {
+  // Clear field type definition cache.
+  \Drupal::service('plugin.manager.field.field_type')->clearCachedDefinitions();
 
-  FieldConfig::create([
-    'label' => 'Timeline',
-    'field_name' => 'oe_timeline',
-    'entity_type' => 'node',
-    'bundle' => 'oe_policy',
-    'settings' => [],
-    'required' => FALSE,
-  ])->save();
+  if (!\Drupal::service('entity_type.manager')->getStorage('field_storage_config')->load('node.oe_timeline')) {
+    FieldStorageConfig::create([
+      'field_name' => 'oe_timeline',
+      'entity_type' => 'node',
+      'type' => 'timeline_field',
+      'cardinality' => -1,
+      'entity_types' => ['node'],
+    ])->save();
+  }
+
+  if (!\Drupal::service('entity_type.manager')->getStorage('field_config')->load('node.oe_policy.oe_timeline')) {
+    FieldConfig::create([
+      'label' => 'Timeline',
+      'field_name' => 'oe_timeline',
+      'entity_type' => 'node',
+      'bundle' => 'oe_policy',
+      'settings' => [],
+      'required' => FALSE,
+    ])->save();
+  }
 }
