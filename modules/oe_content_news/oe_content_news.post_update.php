f1ac1d095952b6a1d566fec460f62a0d3d49b773
imanol <imanoleguskizarojo@gmail.com>
2019-07-29 12:06:20 +0100

    OPENEUROPA-2039: Use new post update method numeration.

--- modules/oe_content_news/oe_content_news.post_update.php
+++ modules/oe_content_news/oe_content_news.post_update.php
@@ -12,7 +12,7 @@ use Drupal\field\Entity\FieldConfig;
 /**
  * Update body and summary labels.
  */
-function oe_content_news_post_update_8100_update_field_labels(array &$sandbox): void {
+function oe_content_news_post_update_00001_update_field_labels(array &$sandbox): void {
   $new_field_labels = [
     'node.oe_news.oe_summary' => 'Introduction',
     'node.oe_news.body' => 'Body text',
