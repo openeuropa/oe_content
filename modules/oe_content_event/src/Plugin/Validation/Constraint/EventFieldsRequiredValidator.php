4e24e035065310e8b05b9168fce0059d65632c27
nagyad <joevagyok@gmail.com>
2019-08-27 15:41:12 +0200

    OPENEUROPA-2200: Improve field validation constraint.

--- modules/oe_content_event/src/Plugin/Validation/Constraint/EventFieldsRequiredValidator.php
+++ modules/oe_content_event/src/Plugin/Validation/Constraint/EventFieldsRequiredValidator.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Drupal\oe_content_event\Plugin\Validation\Constraint;
 
+use Drupal\node\NodeInterface;
 use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Constraint;
 
@@ -14,65 +15,69 @@ class EventFieldsRequiredValidator extends ConstraintValidator {
 
   /**
    * {@inheritdoc}
-   *
-   * @SuppressWarnings(PHPMD.CyclomaticComplexity)
-   * @SuppressWarnings(PHPMD.NPathComplexity)
    */
-  public function validate($items, Constraint $constraint) {
-    /* @var \Drupal\node\NodeInterface $items */
-    if (!isset($items) || $items->getType() !== 'oe_event') {
+  public function validate($node, Constraint $constraint) {
+    /* @var \Drupal\node\NodeInterface $node */
+    if (!isset($node) || $node->getType() !== 'oe_event') {
       return;
     }
 
-    // Check for the "Online" fields to validate.
-    // The "Online time start" and "Online link" fields are NOT required if
-    // the "Online type" is not provided.
-    $required = !empty($items->field_online_type->getValue());
-    if ($required && empty($items->field_online_time_start->getValue())) {
-      $this->context->buildViolation($constraint->message, ['@name' => $items->getFieldDefinition('field_online_time_start')->getLabel()])
-        ->atPath('field_online_time_start')
-        ->addViolation();
-    }
-    if ($required && empty($items->field_online_link->getValue())) {
-      $this->context->buildViolation($constraint->message, ['@name' => $items->getFieldDefinition('field_online_link')->getLabel()])
-        ->atPath('field_online_link')
-        ->addViolation();
+    $online_required_fields = [
+      'field_online_type',
+      'field_online_time_start',
+      'field_online_link',
+    ];
+    // Check if any of these "Online" field group fields are filled in,
+    // then they are all required..
+    $this->validateGroupFieldsEmpty($online_required_fields, $constraint, $node);
+
+    $description_fields_required = [
+      'field_featured_media',
+      'field_featured_media_legend',
+      'body',
+    ];
+    // Check if any of these "Description" field group fields are filled in,
+    // then they are all required.
+    $this->validateGroupFieldsEmpty($description_fields_required, $constraint, $node);
+
+    $registration_fields_required = [
+      'field_registration_url',
+      'field_registration_status',
+      'field_registration_start_date',
+      'field_registration_end_date',
+    ];
+    // Check if any of these "Registration" field group fields are filled in,
+    // then they are all required.
+    $this->validateGroupFieldsEmpty($registration_fields_required, $constraint, $node);
+  }
+
+  /**
+   * Helper function to provide violation on a set of fields that are required.
+   *
+   * @param array $fields
+   *   List of fields to check.
+   * @param \Symfony\Component\Validator\Constraint $constraint
+   *   The constraint object.
+   * @param \Drupal\node\NodeInterface $node
+   *   The node object.
+   */
+  protected function validateGroupFieldsEmpty(array $fields, Constraint $constraint, NodeInterface $node) {
+    $field_values = [];
+
+    // Check for values in each field.
+    foreach ($fields as $field_name) {
+      $field_values[$field_name] = $node->get($field_name)->isEmpty();
     }
 
-    // Check for the "Description" fields to validate.
-    // The "Featured media legend" and the "Body" are NOT required if the
-    // "Featured media" is not provided.
-    $required = !empty($items->field_featured_media->getValue());
-    if ($required && empty($items->field_featured_media_legend->getValue())) {
-      $this->context->buildViolation($constraint->message, ['@name' => $items->getFieldDefinition('field_featured_media_legend')->getLabel()])
-        ->atPath('field_featured_media_legend')
-        ->addViolation();
-    }
-    if ($required && empty($items->body->getValue())) {
-      $this->context->buildViolation($constraint->message, ['@name' => $items->getFieldDefinition('body')->getLabel()])
-        ->atPath('body')
-        ->addViolation();
-    }
-
-    // Check for the "Registration" fields to validate.
-    // The "Registration status", "Registration start date" and the
-    // "Registration end date" are NOT required if the "Registration URL"
-    // is not provided.
-    $required = !empty($items->field_registration_url->getValue());
-    if ($required && empty($items->field_registration_status->getValue())) {
-      $this->context->buildViolation($constraint->message, ['@name' => $items->getFieldDefinition('field_registration_status')->getLabel()])
-        ->atPath('field_registration_status')
-        ->addViolation();
-    }
-    if ($required && empty($items->field_registration_start_date->getValue())) {
-      $this->context->buildViolation($constraint->message, ['@name' => $items->getFieldDefinition('field_registration_start_date')->getLabel()])
-        ->atPath('field_registration_start_date')
-        ->addViolation();
-    }
-    if ($required && empty($items->field_registration_end_date->getValue())) {
-      $this->context->buildViolation($constraint->message, ['@name' => $items->getFieldDefinition('field_registration_end_date')->getLabel()])
-        ->atPath('field_registration_end_date')
-        ->addViolation();
+    // If any of these fields are NOT empty, then all the rest are required.
+    if (in_array(FALSE, $field_values)) {
+      foreach ($field_values as $field_name => $has_value) {
+        if ($has_value) {
+          $this->context->buildViolation($constraint->message, ['@name' => $node->getFieldDefinition($field_name)->getLabel()])
+            ->atPath($field_name)
+            ->addViolation();
+        }
+      }
     }
   }
 
