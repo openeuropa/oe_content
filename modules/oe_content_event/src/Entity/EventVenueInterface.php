a62b2d627cfb5c18614db0dcd17394f1bc8e0171
nagyad <joevagyok@gmail.com>
2019-09-06 12:09:55 +0200

    OPENEUROPA-2111: Add event venue entity.

---
+++ modules/oe_content_event/src/Entity/EventVenueInterface.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Drupal\oe_content_event\Entity;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\RevisionLogInterface;
+use Drupal\Core\Entity\EntityChangedInterface;
+use Drupal\Core\Entity\EntityPublishedInterface;
+use Drupal\user\EntityOwnerInterface;
+
+/**
+ * Provides an interface for defining Event venue entities.
+ *
+ * @ingroup oe_content_event
+ */
+interface EventVenueInterface extends ContentEntityInterface, RevisionLogInterface, EntityChangedInterface, EntityPublishedInterface, EntityOwnerInterface {
+
+  /**
+   * Add get/set methods for your configuration properties here.
+   */
+
+  /**
+   * Gets the Event venue name.
+   *
+   * @return string
+   *   Name of the Event venue.
+   */
+  public function getName();
+
+  /**
+   * Sets the Event venue name.
+   *
+   * @param string $name
+   *   The Event venue name.
+   *
+   * @return \Drupal\oe_content_event\Entity\EventVenueInterface
+   *   The called Event venue entity.
+   */
+  public function setName($name);
+
+  /**
+   * Gets the Event venue creation timestamp.
+   *
+   * @return int
+   *   Creation timestamp of the Event venue.
+   */
+  public function getCreatedTime();
+
+  /**
+   * Sets the Event venue creation timestamp.
+   *
+   * @param int $timestamp
+   *   The Event venue creation timestamp.
+   *
+   * @return \Drupal\oe_content_event\Entity\EventVenueInterface
+   *   The called Event venue entity.
+   */
+  public function setCreatedTime($timestamp);
+
+  /**
+   * Gets the Event venue revision creation timestamp.
+   *
+   * @return int
+   *   The UNIX timestamp of when this revision was created.
+   */
+  public function getRevisionCreationTime();
+
+  /**
+   * Sets the Event venue revision creation timestamp.
+   *
+   * @param int $timestamp
+   *   The UNIX timestamp of when this revision was created.
+   *
+   * @return \Drupal\oe_content_event\Entity\EventVenueInterface
+   *   The called Event venue entity.
+   */
+  public function setRevisionCreationTime($timestamp);
+
+  /**
+   * Gets the Event venue revision author.
+   *
+   * @return \Drupal\user\UserInterface
+   *   The user entity for the revision author.
+   */
+  public function getRevisionUser();
+
+  /**
+   * Sets the Event venue revision author.
+   *
+   * @param int $uid
+   *   The user ID of the revision author.
+   *
+   * @return \Drupal\oe_content_event\Entity\EventVenueInterface
+   *   The called Event venue entity.
+   */
+  public function setRevisionUserId($uid);
+
+}
---
+++ modules/oe_content_event/src/Form/EventVenueForm.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Drupal\oe_content_event\Form;
+
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\Entity\ContentEntityForm;
+use Drupal\Core\Entity\EntityRepositoryInterface;
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Form controller for Event venue edit forms.
+ *
+ * @ingroup oe_content_event
+ */
+class EventVenueForm extends ContentEntityForm {
+
+  /**
+   * The current user account.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $account;
+
+  /**
+   * Constructs a new EventVenueForm.
+   *
+   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
+   *   The entity repository service.
+   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $entity_type_bundle_info
+   *   The entity type bundle service.
+   * @param \Drupal\Component\Datetime\TimeInterface $time
+   *   The time service.
+   * @param \Drupal\Core\Session\AccountProxyInterface $account
+   *   The current user account.
+   */
+  public function __construct(EntityRepositoryInterface $entity_repository, EntityTypeBundleInfoInterface $entity_type_bundle_info = NULL, TimeInterface $time = NULL, AccountProxyInterface $account) {
+    parent::__construct($entity_repository, $entity_type_bundle_info, $time);
+
+    $this->account = $account;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiates this form class.
+    return new static(
+      $container->get('entity.repository'),
+      $container->get('entity_type.bundle.info'),
+      $container->get('datetime.time'),
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    /* @var \Drupal\oe_content_event\Entity\EventVenue $entity */
+    $form = parent::buildForm($form, $form_state);
+
+    if (!$this->entity->isNew()) {
+      $form['new_revision'] = [
+        '#type' => 'checkbox',
+        '#title' => $this->t('Create new revision'),
+        '#default_value' => FALSE,
+        '#weight' => 10,
+      ];
+    }
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function save(array $form, FormStateInterface $form_state) {
+    $entity = $this->entity;
+
+    // Save as a new revision if requested to do so.
+    if (!$form_state->isValueEmpty('new_revision') && $form_state->getValue('new_revision') != FALSE) {
+      $entity->setNewRevision();
+
+      // If a new revision is created, save the current user as revision author.
+      $entity->setRevisionCreationTime($this->time->getRequestTime());
+      $entity->setRevisionUserId($this->account->id());
+    }
+    else {
+      $entity->setNewRevision(FALSE);
+    }
+
+    $status = parent::save($form, $form_state);
+
+    switch ($status) {
+      case SAVED_NEW:
+        $this->messenger()->addMessage($this->t('Created the %label Event venue.', [
+          '%label' => $entity->label(),
+        ]));
+        break;
+
+      default:
+        $this->messenger()->addMessage($this->t('Saved the %label Event venue.', [
+          '%label' => $entity->label(),
+        ]));
+    }
+    $form_state->setRedirect('entity.event_venue.canonical', ['event_venue' => $entity->id()]);
+  }
+
+}
