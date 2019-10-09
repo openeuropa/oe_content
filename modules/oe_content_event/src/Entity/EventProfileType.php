6f59613adeeeb23f3c9771da8983895da266a1fe
nagyad <joevagyok@gmail.com>
2019-10-09 10:42:39 +0200

    OPENEUROPA-2205: Add event profile bundleable entity.

---
+++ modules/oe_content_event/src/Entity/EventProfileType.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_content_event\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityBundleBase;
+
+/**
+ * Defines the Event profile type entity.
+ *
+ * @ConfigEntityType(
+ *   id = "event_profile_type",
+ *   label = @Translation("Event profile Type"),
+ *   bundle_of = "event_profile",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *   },
+ *   config_prefix = "event_profile_type",
+ *   config_export = {
+ *     "id",
+ *     "label",
+ *     "description",
+ *   },
+ *   handlers = {
+ *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
+ *     "list_builder" = "Drupal\oe_content_event\EventProfileTypeListBuilder",
+ *     "form" = {
+ *       "default" = "Drupal\oe_content_event\Form\EventProfileTypeForm",
+ *       "add" = "Drupal\oe_content_event\Form\EventProfileTypeForm",
+ *       "edit" = "Drupal\oe_content_event\Form\EventProfileTypeForm",
+ *       "delete" = "Drupal\oe_content_event\Form\EventProfileTypeDeleteForm",
+ *     },
+ *     "route_provider" = {
+ *       "html" = "Drupal\Core\Entity\Routing\AdminHtmlRouteProvider",
+ *     },
+ *   },
+ *   admin_permission = "administer event profile types",
+ *   links = {
+ *     "canonical" = "/admin/structure/event_profile_type/{event_profile_type}",
+ *     "add-form" = "/admin/structure/event_profile_type/add",
+ *     "edit-form" = "/admin/structure/event_profile_type/{event_profile_type}/edit",
+ *     "delete-form" = "/admin/structure/event_profile_type/{event_profile_type}/delete",
+ *     "collection" = "/admin/structure/event_profile_type",
+ *   }
+ * )
+ */
+class EventProfileType extends ConfigEntityBundleBase {
+  /**
+   * The machine name of the event profile type.
+   *
+   * @var string
+   */
+  protected $id;
+
+  /**
+   * The human-readable name of the event profile type.
+   *
+   * @var string
+   */
+  protected $label;
+
+  /**
+   * A brief description of the event profile type.
+   *
+   * @var string
+   */
+  protected $description;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDescription() {
+    return $this->description;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setDescription(string $description): EventProfileType {
+    $this->description = $description;
+    return $this;
+  }
+
+}
---
+++ modules/oe_content_event/src/Form/EventProfileTypeDeleteForm.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_content_event\Form;
+
+use Drupal\Core\Entity\Query\QueryFactory;
+use Drupal\Core\Entity\EntityDeleteForm;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a form for event profile type deletion.
+ *
+ * @ingroup oe_content_event
+ */
+class EventProfileTypeDeleteForm extends EntityDeleteForm {
+
+  /**
+   * The query factory to create entity queries.
+   *
+   * @var \Drupal\Core\Entity\Query\QueryFactory
+   */
+  protected $queryFactory;
+
+  /**
+   * Constructs a new EventProfileTypeDeleteForm object.
+   *
+   * @param \Drupal\Core\Entity\Query\QueryFactory $query_factory
+   *   The entity query object.
+   */
+  public function __construct(QueryFactory $query_factory) {
+    $this->queryFactory = $query_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('entity.query'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $num_lists = $this->queryFactory
+      ->get('event_profile')
+      ->condition('bundle', $this->entity
+        ->id())
+      ->count()
+      ->execute();
+    if ($num_lists) {
+      $caption = '<p>' . $this
+        ->formatPlural($num_lists, '%type is used by 1 event profile on your site. You can not remove this event profile type until you have removed the %type event profile.', '%type is used by @count event profile on your site. You may not remove %type until you have removed all of the %type event profiles.', [
+          '%type' => $this->entity
+            ->label(),
+        ]) . '</p>';
+      $form['#title'] = $this
+        ->getQuestion();
+      $form['description'] = [
+        '#markup' => $caption,
+      ];
+      return $form;
+    }
+    return parent::buildForm($form, $form_state);
+  }
+
+}
