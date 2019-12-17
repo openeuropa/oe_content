481ffd372a9d5c2877cd14243dab93527b38b9b8
nagyad <joevagyok@gmail.com>
2019-12-17 09:14:28 +0100

    OPENEUROPA-2666: Add organisation entity.

---
+++ modules/oe_content_entity/modules/oe_content_entity_organisation/src/Entity/OrganisationType.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_content_entity_organisation\Entity;
+
+use Drupal\oe_content_entity\Entity\EntityTypeBase;
+
+/**
+ * Defines the organisation type entity.
+ *
+ * @ConfigEntityType(
+ *   id = "oe_organisation_type",
+ *   label = @Translation("Organisation type"),
+ *   bundle_of = "oe_organisation",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *   },
+ *   config_prefix = "oe_organisation_type",
+ *   config_export = {
+ *     "id",
+ *     "label",
+ *     "description",
+ *   },
+ *   handlers = {
+ *     "list_builder" = "Drupal\oe_content_entity\EntityTypeListBuilder",
+ *     "form" = {
+ *       "default" = "Drupal\oe_content_entity\Form\EntityTypeForm",
+ *       "add" = "Drupal\oe_content_entity\Form\EntityTypeForm",
+ *       "edit" = "Drupal\oe_content_entity\Form\EntityTypeForm",
+ *       "delete" = "Drupal\oe_content_entity\Form\EntityTypeDeleteForm",
+ *     },
+ *     "route_provider" = {
+ *       "html" = "Drupal\Core\Entity\Routing\AdminHtmlRouteProvider",
+ *     },
+ *   },
+ *   admin_permission = "manage custom content entities",
+ *   links = {
+ *     "add-form" = "/admin/structure/oe_organisation_type/add",
+ *     "edit-form" = "/admin/structure/oe_organisation_type/{oe_organisation_type}/edit",
+ *     "delete-form" = "/admin/structure/oe_organisation_type/{oe_organisation_type}/delete",
+ *     "collection" = "/admin/structure/oe_organisation_type",
+ *   }
+ * )
+ */
+class OrganisationType extends EntityTypeBase {  }
