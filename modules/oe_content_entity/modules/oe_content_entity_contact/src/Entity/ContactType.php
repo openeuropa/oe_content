19871b2953f883848eeb148d96ca3e77891cb0d1
nagyad <joevagyok@gmail.com>
2019-12-17 09:07:39 +0100

    OPENEUROPA-2666: Add contact entity.

---
+++ modules/oe_content_entity/modules/oe_content_entity_contact/src/Entity/ContactType.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_content_entity_contact\Entity;
+
+use Drupal\oe_content_entity\Entity\EntityTypeBase;
+
+/**
+ * Defines the contact type entity.
+ *
+ * @ConfigEntityType(
+ *   id = "oe_contact_type",
+ *   label = @Translation("Contact type"),
+ *   bundle_of = "oe_contact",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *   },
+ *   config_prefix = "oe_contact_type",
+ *   config_export = {
+ *     "id",
+ *     "label",
+ *     "description",
+ *   },
+ *   handlers = {
+ *     "list_builder" = "Drupal\oe_content_entity\EntityTypeListBuilder",
+ *     "form" = {
+ *       "default" = "Drupal\oe_content_entity\Form\EntityTypeForm",
+ *       "add" = "Drupal\oe_content_entity\Form\EntityTypeForm",
+ *       "edit" = "Drupal\oe_content_entity\Form\EntityTypeForm",
+ *       "delete" = "Drupal\oe_content_entity\Form\EntityTypeDeleteForm",
+ *     },
+ *     "route_provider" = {
+ *       "html" = "Drupal\Core\Entity\Routing\AdminHtmlRouteProvider",
+ *     },
+ *   },
+ *   admin_permission = "manage custom content entities",
+ *   links = {
+ *     "add-form" = "/admin/structure/oe_contact_type/add",
+ *     "edit-form" = "/admin/structure/oe_contact_type/{oe_contact_type}/edit",
+ *     "delete-form" = "/admin/structure/oe_contact_type/{oe_contact_type}/delete",
+ *     "collection" = "/admin/structure/oe_contact_type",
+ *   }
+ * )
+ */
+class ContactType extends EntityTypeBase {  }
---
+++ modules/oe_content_entity/modules/oe_content_entity_contact/tests/Kernel/ContactTest.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_content_entity\Kernel;
+
+use Drupal\Tests\rdf_entity\Kernel\RdfKernelTestBase;
+
+/**
+ * Tests the venue entity.
+ */
+class ContactTest extends RdfKernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'oe_content',
+    'oe_content_entity',
+    'oe_content_entity_contact',
+    'entity_reference_revisions',
+    'inline_entity_form',
+    'node',
+    'user',
+    'maxlength',
+    'menu_ui',
+    'media',
+    'options',
+    'address',
+    'field',
+    'field_group',
+    'image',
+    'link',
+    'typed_link',
+    'rdf_skos',
+    'system',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $this->installEntitySchema('user');
+    $this->installEntitySchema('node');
+    $this->installEntitySchema('media');
+    $this->installEntitySchema('oe_contact');
+    $this->installSchema('node', 'node_access');
+    $this->installConfig([
+      'address',
+      'field',
+      'field_group',
+      'filter',
+      'link',
+      'typed_link',
+      'maxlength',
+      'node',
+      'system',
+      'rdf_skos',
+      'oe_content',
+      'oe_content_entity_contact',
+    ]);
+    module_load_include('install', 'oe_content');
+    oe_content_install();
+  }
+
+  /**
+   * Tests contact entities.
+   */
+  public function testContact(): void {
+    /** @var \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager */
+    $entity_type_manager = $this->container->get('entity_type.manager');
+
+    // Create a contact type.
+    $contact_type_storage = $entity_type_manager->getStorage('oe_contact_type');
+    $contact_type = $contact_type_storage->create(['label' => 'Test contact type', 'id' => 'test_contact_type']);
+    $contact_type->save();
+
+    $contact_type = $contact_type_storage->load($contact_type->id());
+    $this->assertEquals('Test contact type', $contact_type->label());
+    $this->assertEquals('test_contact_type', $contact_type->id());
+
+    // Create a contact.
+    $contact_storage = $entity_type_manager->getStorage('oe_contact');
+    $values = [
+      'bundle' => $contact_type->id(),
+      'name' => 'My contact',
+    ];
+    /** @var \Drupal\oe_content_event\Entity\EventProfileInterface $event_profile */
+    $event_profile = $contact_storage->create($values);
+    $event_profile->save();
+
+    $event_profile = $contact_storage->load($event_profile->id());
+    $this->assertEquals('My contact', $event_profile->getName());
+    $this->assertEquals($contact_type->id(), $event_profile->bundle());
+  }
+
+}
