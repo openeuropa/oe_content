e9562b408b257b1d49b207516bd96afe692ade88
nagyad <joevagyok@gmail.com>
2019-12-23 16:06:11 +0100

    OPENEUROPA-2666: Minor improvements.

--- modules/oe_content_entity/modules/oe_content_entity_organisation/src/Entity/Organisation.php
+++ modules/oe_content_entity/modules/oe_content_entity_organisation/src/Entity/Organisation.php
@@ -14,6 +14,7 @@ use Drupal\oe_content_entity\Entity\EntityBase;
  * @ContentEntityType(
  *   id = "oe_organisation",
  *   label = @Translation("Organisation"),
+ *   label_collection = @Translation("Organisations"),
  *   bundle_label = @Translation("Organisation type"),
  *   handlers = {
  *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
@@ -35,6 +36,7 @@ use Drupal\oe_content_entity\Entity\EntityBase;
  *   revision_table = "oe_organisation_revision",
  *   revision_data_table = "oe_organisation_field_revision",
  *   translatable = TRUE,
+ *   show_revision_ui = TRUE,
  *   admin_permission = "manage custom content entities",
  *   entity_keys = {
  *     "id" = "id",
