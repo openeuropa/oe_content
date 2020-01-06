5e43fec0ddee769bf9959a6747fc80d108b1ac61
Antonio De Marco <antonio@nuvole.org>
2020-01-06 15:41:27 +0100

    OPENEUROPA-2666: Remove useless new_revision form element.

--- modules/oe_content_entity/src/Form/EntityForm.php
+++ modules/oe_content_entity/src/Form/EntityForm.php
@@ -41,7 +41,7 @@ class EntityForm extends ContentEntityForm {
    * @param \Drupal\Core\Messenger\MessengerInterface $messenger
    *   The messenger.
    */
-  public function __construct(EntityRepositoryInterface $entity_repository, EntityTypeBundleInfoInterface $entity_type_bundle_info = NULL, TimeInterface $time = NULL, AccountProxyInterface $account, MessengerInterface $messenger) {
+  public function __construct(EntityRepositoryInterface $entity_repository, EntityTypeBundleInfoInterface $entity_type_bundle_info, TimeInterface $time, AccountProxyInterface $account, MessengerInterface $messenger) {
     parent::__construct($entity_repository, $entity_type_bundle_info, $time);
     $this->account = $account;
     $this->messenger = $messenger;
@@ -63,35 +63,9 @@ class EntityForm extends ContentEntityForm {
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state): array {
-    $form = parent::buildForm($form, $form_state);
-    if (!$this->entity->isNew()) {
-      $form['new_revision'] = [
-        '#type' => 'checkbox',
-        '#title' => $this->t('Create new revision'),
-        '#default_value' => TRUE,
-        '#weight' => 10,
-      ];
-    }
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function save(array $form, FormStateInterface $form_state): void {
-    $entity = $this->entity;
-    // Save as a new revision if requested to do so.
-    if (!$form_state->isValueEmpty('new_revision') && (bool) $form_state->getValue('new_revision') === FALSE) {
-      $entity->setNewRevision(FALSE);
-    }
-    else {
-      $entity->setNewRevision();
-      // If a new revision is created, save also the revision metadata.
-      $entity->setRevisionCreationTime($this->time->getRequestTime());
-      $entity->setRevisionUserId($this->account->id());
-    }
     $status = parent::save($form, $form_state);
+    $entity = $this->entity;
     switch ($status) {
       case SAVED_NEW:
         $this->messenger->addMessage($this->t('Created the %label.', [
