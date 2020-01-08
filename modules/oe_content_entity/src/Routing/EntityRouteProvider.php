dba27c01eb63609fae75aba55b93e8bc134bf2b7
nagyad <joevagyok@gmail.com>
2020-01-08 11:53:11 +0100

    OPENEUROPA-2666: Cleanup.

--- modules/oe_content_entity/src/Routing/EntityRouteProvider.php
+++ modules/oe_content_entity/src/Routing/EntityRouteProvider.php
@@ -1,34 +1,18 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Drupal\oe_content_entity\Routing;
 
-use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Entity\Routing\AdminHtmlRouteProvider;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Provides HTML routes for corporate content entity pages.
+ * We need to override canonical to serve edit page for corporate entities.
+ *
+ * These entities are not meant to have accessible canonical urls.
  */
 class EntityRouteProvider extends AdminHtmlRouteProvider {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, EntityFieldManagerInterface $entity_field_manager) {
-    parent::__construct($entity_type_manager, $entity_field_manager);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
-    return new static(
-      $container->get('entity_type.manager'),
-      $container->get('entity_field.manager')
-    );
-  }
 
   /**
    * {@inheritdoc}
