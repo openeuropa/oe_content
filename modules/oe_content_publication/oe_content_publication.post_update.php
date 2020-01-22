be3a1a3c10205801e5f58edb12bf86e7a8ec34a3
22Alexandra <beldean.ale@gmail.com>
2020-01-22 13:57:58 +0100

    OPENEUROPA-2730: Remove upgrade path for fields updates.

--- modules/oe_content_publication/oe_content_publication.post_update.php
+++ modules/oe_content_publication/oe_content_publication.post_update.php
@@ -22,28 +22,3 @@ function oe_content_publication_post_update_00001_update_field_labels(array &$sa
     $field_config->save();
   }
 }
-
-/**
- * Update title, teaser, summary and subject fields description.
- */
-function oe_content_publication_post_update_00002(array &$sandbox): void {
-  // Update teaser, summary and subject.
-  $fields_description = [
-    'node.oe_publication.oe_subject' => 'The topics mentioned on this page. These will be used by search engines and dynamic lists to determine their relevance to a user.',
-    'node.oe_publication.oe_summary' => 'A short text that will be displayed in the blue header, below the page title. This should be a brief summary of the content on the page that tells the user what information they will find on this page.',
-    'node.oe_publication.oe_teaser' => 'A short overview of the information on this page. The teaser will be displayed in list views and search engine results, not on the page itself. Limited to 150 characters for SEO purposes.',
-  ];
-
-  foreach ($fields_description as $id => $description) {
-    $field_config = FieldConfig::load($id);
-    $field_config->setDescription($description);
-    $field_config->save();
-  }
-
-  // Update title base field.
-  $fields = \Drupal::service('entity_field.manager')->getBaseFieldDefinitions('node', 'oe_publication');
-  $field_config = $fields['title']->getConfig('oe_publication');
-  $field_config->setLabel('Page title');
-  $field_config->setDescription('The ideal length is 50 to 60 characters including spaces. If it must be longer, make sure you fill in a shorter version in the Alternative title field.');
-  $field_config->save();
-}
