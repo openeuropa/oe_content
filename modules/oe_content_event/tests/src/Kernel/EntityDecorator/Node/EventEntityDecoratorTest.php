a89c900ecd351d4e9b029473f58729236cc014a0
Antonio De Marco <antonio@nuvole.org>
2020-01-24 09:35:44 +0100

    OPENEUROPA-2221: Add registration period methods.

--- modules/oe_content_event/tests/src/Kernel/EntityDecorator/Node/EventEntityDecoratorTest.php
+++ modules/oe_content_event/tests/src/Kernel/EntityDecorator/Node/EventEntityDecoratorTest.php
@@ -18,9 +18,51 @@ class EventEntityDecoratorTest extends EventKernelTestBase {
    * Test decorator methods.
    */
   public function testDecoratorMethods(): void {
-    foreach ($this->decoratorAssertions() as $assertion) {
+    foreach ($this->decoratorGettersAssertions() as $assertion) {
       $this->assertDecoratorMethods($assertion['case'], $assertion['values'], $assertion['assertions']);
     }
+  }
+
+  /**
+   * Test registration period methods.
+   */
+  public function testRegistrationPeriodMethods(): void {
+    $decorator = $this->createDecorator([
+      'oe_event_registration_dates' => [
+        'value' => '2016-05-10T12:00:00',
+        'end_value' => '2016-05-15T12:00:00',
+      ],
+    ]);
+
+    // Registration yet to come.
+    $now = \DateTime::createFromFormat(DrupalDateTime::FORMAT, '2016-05-09 12:00:00', new \DateTimeZone('UTC'));
+    $this->assertEquals(TRUE, $decorator->isRegistrationPeriodYetToCome($now));
+    $this->assertEquals(FALSE, $decorator->isRegistrationPeriodActive($now));
+    $this->assertEquals(FALSE, $decorator->isRegistrationPeriodOver($now));
+
+    // Registration just started.
+    $now = \DateTime::createFromFormat(DrupalDateTime::FORMAT, '2016-05-10 12:00:00', new \DateTimeZone('UTC'));
+    $this->assertEquals(FALSE, $decorator->isRegistrationPeriodYetToCome($now));
+    $this->assertEquals(TRUE, $decorator->isRegistrationPeriodActive($now));
+    $this->assertEquals(FALSE, $decorator->isRegistrationPeriodOver($now));
+
+    // Registration in progress.
+    $now = \DateTime::createFromFormat(DrupalDateTime::FORMAT, '2016-05-12 12:00:00', new \DateTimeZone('UTC'));
+    $this->assertEquals(FALSE, $decorator->isRegistrationPeriodYetToCome($now));
+    $this->assertEquals(TRUE, $decorator->isRegistrationPeriodActive($now));
+    $this->assertEquals(FALSE, $decorator->isRegistrationPeriodOver($now));
+
+    // Registration just ended.
+    $now = \DateTime::createFromFormat(DrupalDateTime::FORMAT, '2016-05-15 12:00:00', new \DateTimeZone('UTC'));
+    $this->assertEquals(FALSE, $decorator->isRegistrationPeriodYetToCome($now));
+    $this->assertEquals(FALSE, $decorator->isRegistrationPeriodActive($now));
+    $this->assertEquals(TRUE, $decorator->isRegistrationPeriodOver($now));
+
+    // Registration is over.
+    $now = \DateTime::createFromFormat(DrupalDateTime::FORMAT, '2016-05-20 12:00:00', new \DateTimeZone('UTC'));
+    $this->assertEquals(FALSE, $decorator->isRegistrationPeriodYetToCome($now));
+    $this->assertEquals(FALSE, $decorator->isRegistrationPeriodActive($now));
+    $this->assertEquals(TRUE, $decorator->isRegistrationPeriodOver($now));
   }
 
   /**
@@ -35,12 +77,7 @@ class EventEntityDecoratorTest extends EventKernelTestBase {
    */
   protected function assertDecoratorMethods(string $case, array $values, array $assertions): void {
     // Create decorator.
-    $node = Node::create($values + [
-      'type' => 'oe_event',
-      'title' => 'My event',
-    ]);
-    $node->save();
-    $decorator = new EventEntityDecorator($node);
+    $decorator = $this->createDecorator($values);
 
     // Run assertions.
     foreach ($assertions as $method => $expected) {
@@ -53,7 +90,26 @@ class EventEntityDecoratorTest extends EventKernelTestBase {
   }
 
   /**
-   * Assertion for decorator test.
+   * Create a decorator object given its node entity values.
+   *
+   * @param array $values
+   *   Entity values.
+   *
+   * @return \Drupal\oe_content_event\EntityDecorator\Node\EventEntityDecorator
+   *   Decorator object.
+   */
+  protected function createDecorator(array $values): EventEntityDecorator {
+    // Create decorator.
+    $node = Node::create($values + [
+      'type' => 'oe_event',
+      'title' => 'My event',
+    ]);
+    $node->save();
+    return new EventEntityDecorator($node);
+  }
+
+  /**
+   * Assertion for decorator getters.
    *
    * This would normally be a data provider but it would require a full test
    * bootstrap for each test case, which will add minutes to test runs.
@@ -64,7 +120,7 @@ class EventEntityDecoratorTest extends EventKernelTestBase {
    * @return array
    *   List of assertion for decorator test.
    */
-  public function decoratorAssertions(): array {
+  public function decoratorGettersAssertions(): array {
     return [
       [
         'case' => 'Test default getters behaviour when no fields are set',
@@ -129,6 +185,28 @@ class EventEntityDecoratorTest extends EventKernelTestBase {
         'case' => 'Test closed registration',
         'values' => [
           'oe_event_registration_status' => 'closed',
+        ],
+        'assertions' => [
+          'isRegistrationOpen' => FALSE,
+          'isRegistrationClosed' => TRUE,
+        ],
+      ],
+      [
+        'case' => 'Test open registration with cancelled event',
+        'values' => [
+          'oe_event_registration_status' => 'open',
+          'oe_event_status' => 'cancelled',
+        ],
+        'assertions' => [
+          'isRegistrationOpen' => FALSE,
+          'isRegistrationClosed' => TRUE,
+        ],
+      ],
+      [
+        'case' => 'Test open registration with postponed event',
+        'values' => [
+          'oe_event_registration_status' => 'open',
+          'oe_event_status' => 'postponed',
         ],
         'assertions' => [
           'isRegistrationOpen' => FALSE,
