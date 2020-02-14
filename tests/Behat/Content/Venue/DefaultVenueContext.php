4e3969a9964dd45aed0d45c1596fe60e8d54b09c
nagyad <joevagyok@gmail.com>
2020-02-14 09:24:10 +0100

    OPENEUROPA-2221: Create contexts with mapping for corporate entities.

---
+++ tests/Behat/Content/Venue/DefaultVenueContext.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_content\Behat\Content\Venue;
+
+use Drupal\Tests\oe_content\Behat\Content\RawCorporateContentContext;
+use Drupal\Tests\oe_content\Behat\Hook\Scope\BeforeParseEntityFieldsScope;
+
+/**
+ * Context to create venue corporate entities.
+ */
+class DefaultVenueContext extends RawCorporateContentContext {
+
+  /**
+   * Run before fields are parsed by Drupal Behat extension.
+   *
+   * @BeforeParseEntityFields(oe_venue,oe_default)
+   */
+  public function alterVenueFields(BeforeParseEntityFieldsScope $scope): void {
+    $fields = [];
+
+    // Maps human readable field names to their Behat parsable machine names.
+    $mapping = [
+      'Name' => 'name',
+      'Address' => 'oe_address',
+      'Capacity' => 'oe_capacity',
+      'Room' => 'oe_room',
+    ];
+    foreach ($scope->getFields() as $key => $value) {
+      $key = $mapping[$key] ?? $key;
+      $fields[$key] = $value;
+    }
+
+    $scope->setFields($fields);
+  }
+
+}
