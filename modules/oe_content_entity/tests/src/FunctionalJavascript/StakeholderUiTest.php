f125938e930971a3007546e58e507a71812ab550
Balint Junkuncz <junkuncz@gmail.com>
2020-05-14 14:07:23 +0200

    OPENEUROPA-3113: Added more tests.

--- modules/oe_content_entity/tests/src/FunctionalJavascript/StakeholderUiTest.php
+++ modules/oe_content_entity/tests/src/FunctionalJavascript/StakeholderUiTest.php
@@ -40,29 +40,41 @@ class StakeholderUiTest extends BrowserTestBase {
 
     $this->getSession()->getPage()->fillField('Name', "My stakeholder");
     $acronym_value = "My Acronym";
+    $company_value = "Best company";
+    $country = "HU";
+    $street_address_value = "My street 20.";
+    $city_value = "Budapest";
+    $postal_code_value = "1171";
+    $website_value = "https://test.com";
+    $contact_value = "https://test2.com";
     $this->getSession()->getPage()->fillField('Acronym', $acronym_value);
-    $this->getSession()->getPage()->fillField('Street address', "Kossuth u. 120.");
-    $this->getSession()->getPage()->fillField('Country', "HU");
-    $this->getSession()->getPage()->fillField('City', "Budapest");
-    $this->getSession()->getPage()->fillField('Company', "Best company");
-    $this->getSession()->getPage()->fillField('Postal code', "1171");
-    $this->getSession()->getPage()->fillField('Website', "https://test.com");
-    $this->getSession()->getPage()->fillField('Contact page URL', "https://test2.com");
+    $this->getSession()->getPage()->fillField('Country', $country);
+    $this->getSession()->getPage()->fillField('Company', $company_value);
+    $this->getSession()->getPage()->fillField('Street address', $street_address_value);
+    $this->getSession()->getPage()->fillField('City', $city_value);
+    $this->getSession()->getPage()->fillField('Postal code', $postal_code_value);
+    $this->getSession()->getPage()->fillField('Website', $website_value);
+    $this->getSession()->getPage()->fillField('Contact page URL', $contact_value);
     $this->getSession()->getPage()->pressButton('Save');
     $this->assertSession()->pageTextContains("Created the My stakeholder.");
 
     $this->drupalGet("/admin/content/oe_organisation/1/edit");
     $this->assertSession()->pageTextContains("My stakeholder");
     $acronym = $this->assertSession()->fieldExists('oe_acronym[0][value]')->getValue();
+    $company = $this->assertSession()->fieldExists('oe_address[0][address][organization]')->getValue();
+    $street_address = $this->assertSession()->fieldExists('oe_address[0][address][address_line1]')->getValue();
+    $city = $this->assertSession()->fieldExists('oe_address[0][address][locality]')->getValue();
+    $postal_code = $this->assertSession()->fieldExists('oe_address[0][address][postal_code]')->getValue();
+    $website = $this->assertSession()->fieldExists('oe_website[0][uri]')->getValue();
+    $contact = $this->assertSession()->fieldExists('oe_contact_url[0][uri]')->getValue();
     $this->assertTrue($acronym == $acronym_value);
-    // $this->assertSession()->pageTextContains("Kossuth u. 120.");
-    $this->assertOptionSelected('Country', 'HU');
-    // $this->assertSession()->pageTextContains("Hungary");
-    // $this->assertSession()->pageTextContains("Budapest");
-    // $this->assertSession()->pageTextContains("12345");
-    // $this->assertSession()->pageTextContains("Best company");
-    // $this->assertSession()->pageTextContains("https://test.com");
-    // $this->assertSession()->pageTextContains("https://test2.com");
+    $this->assertTrue($company == $company_value);
+    $this->assertTrue($street_address == $street_address_value);
+    $this->assertTrue($city == $city_value);
+    $this->assertTrue($postal_code == $postal_code_value);
+    $this->assertTrue($website == $website_value);
+    $this->assertTrue($contact == $contact_value);
+    $this->assertOptionSelected('Country', $country);
   }
 
 }
