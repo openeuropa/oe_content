735bdfd0d42c4995dea3b133a1ffb23b7774336a
Tekrane Julien <julien.tekrane@laposte.net>
2020-06-08 09:18:13 +0000

    OPENEUROPA-3114: Add Behat context for Stakeholder organisations.

--- tests/Behat/Content/Node/ProjectContentContext.php
+++ tests/Behat/Content/Node/ProjectContentContext.php
@@ -4,38 +4,96 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\oe_content\Behat\Content\Node;
 
-use Behat\Gherkin\Node\TableNode;
 use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Drupal\Tests\oe_content\Behat\Hook\Scope\BeforeParseEntityFieldsScope;
+use Drupal\Tests\oe_content\Traits\EntityLoadingTrait;
+use Drupal\Tests\oe_content\Traits\EntityReferenceRevisionTrait;
+use Drupal\Tests\oe_content\Traits\EntityReferenceTrait;
 
 /**
  * Context to create project content entities.
+ *
+ * @SuppressWarnings(PHPMD)
  */
 class ProjectContentContext extends RawDrupalContext {
 
-  /**
-   * Creates stakeholder/s for organisation entity.
-   *
-   * Usage example:
-   *
-   * Given the following stakeholders:
-   *   | name   |
-   *   | name 2 |
-   *   |   ...  |
-   *
-   * @Given the following stakeholder(s):
-   */
-  public function givenStakeholder(TableNode $table):void {
-    $stakeholders = $table->getColumnsHash();
-    $storage = \Drupal::service('entity_type.manager')->getStorage('oe_organisation');
+  use EntityReferenceRevisionTrait;
+  use EntityReferenceTrait;
+  use EntityLoadingTrait;
 
-    foreach ($stakeholders as $stakeholder) {
-      $entity = $storage->create([
-        'bundle' => 'oe_stakeholder',
-        'name' => $stakeholder['name'],
-        'status' => 1,
-      ]);
-      $entity->save();
+  /**
+   * Run before fields are parsed by Drupal Behat extension.
+   *
+   * @param \Drupal\Tests\oe_content\Behat\Hook\Scope\BeforeParseEntityFieldsScope $scope
+   *   Behat hook scope.
+   *
+   * @BeforeParseEntityFields(node,oe_project)
+   */
+  public function alterProjectFields(BeforeParseEntityFieldsScope $scope): void {
+    // Map human readable field names to their Behat parsable machine names.
+    $mapping = [
+      'Alternative title' => 'oe_content_short_title',
+      'Body text' => 'body',
+      'Call for proposals' => 'oe_project_calls',
+      'Coordinators' => 'oe_project_coordinators',
+      'Departments' => 'oe_project_departments',
+      'EU contribution' => 'oe_project_budget_eu',
+      'Funding programme' => 'oe_project_funding_programme',
+      'Navigation title' => 'oe_content_navigation_title',
+      'Overall budget' => 'oe_project_budget',
+      'Participants' => 'oe_project_participants',
+      'Project period' => 'oe_project_dates',
+      'Published' => 'status',
+      'Reference' => 'oe_reference',
+      'Result files' => 'oe_project_result_files',
+      'Results' => 'oe_project_results',
+      'Summary' => 'oe_summary',
+      'Teaser' => 'oe_teaser',
+      'Title' => 'title',
+      'Website' => 'oe_project_website',
+    ];
+
+    foreach ($scope->getFields() as $key => $value) {
+      switch ($key) {
+        // Set SKOS Concemt entity reference fields.
+        case 'Departments':
+        case 'Funding programme':
+          $fields = $this->getRevisionField($mapping[$key], 'skos_concept', $value);
+          $scope->addFields($fields)->removeField($key);
+          break;
+
+        // Set entity_reference_revisions fields.
+        case 'Result files':
+          $fields = $this->getReferenceRevisionField($mapping[$key], 'media', $value);
+          $scope->addFields($fields)->removeField($key);
+          break;
+
+        case 'Coordinators':
+        case 'Participants':
+          $fields = $this->getReferenceRevisionField($mapping[$key], 'oe_organisation', $value);
+          $scope->addFields($fields)->removeField($key);
+          break;
+
+        // Set content published status.
+        case 'Published':
+          $scope->addFields([
+            $mapping[$key] => (int) ($value === 'Yes'),
+          ])->removeField($key);
+          break;
+
+        default:
+          if (isset($mapping[$key])) {
+            $scope->renameField($key, $mapping[$key]);
+          }
+      }
     }
+
+    // Set default fields.
+    $scope->addFields([
+      'oe_subject' => 'http://data.europa.eu/uxp/10',
+      'oe_author' => 'http://publications.europa.eu/resource/authority/corporate-body/AASM',
+      'oe_content_content_owner' => 'http://publications.europa.eu/resource/authority/corporate-body/AASM',
+    ]);
   }
 
 }
---
+++ tests/Behat/Content/Organisation/StakeholderOrganisationContext.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_content\Behat\Content\Organisation;
+
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Drupal\Tests\oe_content\Behat\Hook\Scope\BeforeParseEntityFieldsScope;
+use Drupal\Tests\oe_content\Traits\EntityLoadingTrait;
+use Drupal\Tests\oe_content\Traits\EntityReferenceRevisionTrait;
+use Drupal\Tests\oe_content\Traits\EntityReferenceTrait;
+
+/**
+ * Context to create stakeholder organisation corporate entities.
+ */
+class StakeholderOrganisationContext extends RawDrupalContext {
+
+  use EntityReferenceRevisionTrait;
+  use EntityReferenceTrait;
+  use EntityLoadingTrait;
+
+  /**
+   * Run before fields are parsed by Drupal Behat extension.
+   *
+   * @param \Drupal\Tests\oe_content\Behat\Hook\Scope\BeforeParseEntityFieldsScope $scope
+   *   Behat hook scope.
+   *
+   * @BeforeParseEntityFields(oe_organisation,oe_stakeholder)
+   */
+  public function alterStakeholderOrganisationFields(BeforeParseEntityFieldsScope $scope): void {
+    // Maps human readable field names to their Behat parsable machine names.
+    $mapping = [
+      'Name' => 'name',
+      'Acronym' => 'oe_acronym',
+      'Address' => 'oe_address',
+      'Contact page URL' => 'oe_contact_url',
+      'Logo' => 'oe_logo',
+      'Website' => 'oe_website',
+      'Published' => 'status',
+    ];
+
+    foreach ($scope->getFields() as $key => $value) {
+      switch ($key) {
+        case 'Published':
+          $scope->addFields([
+            $mapping[$key] => (int) ($value === 'Yes'),
+          ])->removeField($key);
+          break;
+
+        case 'Logo':
+          $fields = $this->getRevisionField($mapping[$key], 'media', $value);
+          $scope->addFields($fields)->removeField($key);
+          break;
+
+        default:
+          if (isset($mapping[$key])) {
+            $scope->renameField($key, $mapping[$key]);
+          }
+      }
+    }
+  }
+
+}
