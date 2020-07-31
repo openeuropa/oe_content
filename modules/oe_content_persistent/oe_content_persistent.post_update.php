112608ef2d1ae4427a6b3e2cd27ae869e65ae005
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2020-07-31 17:42:16 +0200

    EWPP-94: Fix issue in post_update.

--- modules/oe_content_persistent/oe_content_persistent.post_update.php
+++ modules/oe_content_persistent/oe_content_persistent.post_update.php
@@ -11,7 +11,9 @@ declare(strict_types = 1);
  * Update PURL configuration.
  */
 function oe_content_persistent_post_update_00001(): void {
-  $supported_entity_types = \Drupal::getContainer()->getParameter('oe_content_persistent.supported_entity_types');
+  if (\Drupal::getContainer()->hasParameter('oe_content_persistent.supported_entity_types')) {
+    $supported_entity_types = \Drupal::getContainer()->getParameter('oe_content_persistent.supported_entity_types');
+  }
   if (empty($supported_entity_types)) {
     $supported_entity_types = ['node'];
   }
