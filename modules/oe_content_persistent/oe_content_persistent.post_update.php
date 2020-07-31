f6aaf51fb0f4ac80001373ab4764466d068101fe
Imanol <ier@opendeusto.es>
2020-07-31 10:59:42 +0100

    EWPP-94: Make sure overriden parameters are taken into account.

--- modules/oe_content_persistent/oe_content_persistent.post_update.php
+++ modules/oe_content_persistent/oe_content_persistent.post_update.php
@@ -11,7 +11,12 @@ declare(strict_types = 1);
  * Update PURL configuration.
  */
 function oe_content_persistent_post_update_00001(): void {
+  $supported_entity_types = \Drupal::getContainer()->getParameter('oe_content_persistent.supported_entity_types');
+  if (empty($supported_entity_types)) {
+    $supported_entity_types = ['node'];
+  }
+  $supported_entity_types = array_combine($supported_entity_types, $supported_entity_types);
   $purl_config = \Drupal::configFactory()->getEditable('oe_content_persistent.settings');
-  $purl_config->set('supported_entity_types', ['node' => 'node']);
+  $purl_config->set('supported_entity_types', $supported_entity_types);
   $purl_config->save();
 }
