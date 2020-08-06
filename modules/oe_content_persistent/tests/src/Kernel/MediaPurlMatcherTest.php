31833a8cd1e7e42364e34012d0cb92958e8061b2
Imanol <ier@opendeusto.es>
2020-08-06 08:14:58 +0100

    EWPP-95: Simplify test.

--- modules/oe_content_persistent/tests/src/Kernel/MediaPurlMatcherTest.php
+++ modules/oe_content_persistent/tests/src/Kernel/MediaPurlMatcherTest.php
@@ -4,27 +4,79 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\oe_content_persistent\Kernel;
 
-use Drupal\Tests\linkit\Kernel\Matchers\MediaMatcherTest;
+use Drupal\file\Entity\File;
+use Drupal\media\Entity\Media;
+use Drupal\media\Entity\MediaType;
+use Drupal\Tests\linkit\Kernel\LinkitKernelTestBase;
 
 /**
  * Tests media PURL matcher.
  */
-class MediaPurlMatcherTest extends MediaMatcherTest {
+class MediaPurlMatcherTest extends LinkitKernelTestBase {
 
   /**
    * Modules to enable.
    *
    * @var array
    */
-  public static $modules = ['oe_content_persistent'];
+  public static $modules = [
+    'file_test',
+    'file',
+    'media',
+    'image',
+    'field',
+    'oe_content_persistent',
+  ];
 
   /**
    * {@inheritdoc}
    */
   protected function setUp() {
     parent::setUp();
-    $this->installConfig(['image']);
+
+    $this->installEntitySchema('file');
+    $this->installEntitySchema('media');
     $this->installEntitySchema('entity_view_display');
+    $this->installConfig(['media']);
+    $this->installConfig(['image']);
+    $this->installSchema('system', ['key_value_expire']);
+    $this->installSchema('file', ['file_usage']);
+
+    // Set up media bundle and fields.
+    $media_type = MediaType::create([
+      'label' => 'test',
+      'id' => 'test',
+      'description' => 'Test type.',
+      'source' => 'file',
+    ]);
+    $media_type->save();
+    $source_field = $media_type->getSource()->createSourceField($media_type);
+    $source_field->getFieldStorageDefinition()->save();
+    $source_field->save();
+    $media_type->set('source_configuration', [
+      'source_field' => $source_field->getName(),
+    ])->save();
+
+    // Linkit doesn't care about the actual resource, only the entity.
+    foreach (['gif', 'jpg', 'png'] as $ext) {
+      $file = File::create([
+        'uid' => 1,
+        'filename' => 'image-test.' . $ext,
+        'uri' => 'public://image-test.' . $ext,
+        'filemime' => 'text/plain',
+        'status' => FILE_STATUS_PERMANENT,
+      ]);
+      $file->save();
+
+      $media = Media::create([
+        'bundle' => 'test',
+        $source_field->getName() => ['target_id' => $file->id()],
+      ]);
+      $media->save();
+    }
+
+    // Create user 1 who has special permissions.
+    \Drupal::currentUser()->setAccount($this->createUser(['uid' => 1]));
   }
 
   /**
@@ -32,9 +84,10 @@ class MediaPurlMatcherTest extends MediaMatcherTest {
    */
   public function testMediaMatcherWithDefaultConfiguration() {
     $base_url = $this->config('oe_content_persistent.settings')->get('base_url');
+    $matcher_manager = $this->container->get('plugin.manager.linkit.matcher');
 
     /** @var \Drupal\linkit\MatcherInterface $plugin */
-    $plugin = $this->manager->createInstance('entity:media', []);
+    $plugin = $matcher_manager->createInstance('entity:media', []);
     $suggestions = $plugin->execute('image-test');
     $this->assertEquals(3, count($suggestions->getSuggestions()), 'Correct number of suggestions.');
 
@@ -47,7 +100,7 @@ class MediaPurlMatcherTest extends MediaMatcherTest {
     }
 
     /** @var \Drupal\linkit\MatcherInterface $plugin */
-    $plugin = $this->manager->createInstance('entity:media', [
+    $plugin = $matcher_manager->createInstance('entity:media', [
       'settings' => [
         'thumbnail' => [
           'show_thumbnail' => TRUE,
