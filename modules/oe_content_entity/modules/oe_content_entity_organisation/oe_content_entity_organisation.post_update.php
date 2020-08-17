51eb687bcec5f1fd110726aa45df4f6d708dec74
Enrique Lacoma <enriquelacoma@gmail.com>
2020-08-17 14:21:51 +0000

    EWPP-166: Add post_update to oe_content_entity_organization and run it's updates when enabling oe_content_project.

---
+++ modules/oe_content_entity/modules/oe_content_entity_organisation/oe_content_entity_organisation.post_update.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Post update functions for OpenEuropa Content Entity Organisation module.
+ */
+
+declare(strict_types = 1);
+
+use Drupal\Core\Config\FileStorage;
+
+/**
+ * Update configuration for oe_stakeholder.
+ */
+function oe_content_entity_organisation_post_update_00001(): void {
+  // Obtain configuration from yaml files.
+  $storage = new FileStorage(drupal_get_path('module', 'oe_content_entity_organisation') . '/config/post_updates/0001_oe_stakeholder');
+  \Drupal::service('config.installer')->installOptionalConfig($storage);
+}
