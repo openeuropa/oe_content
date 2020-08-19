7524855deb2481d213ed41e601372c9f95543798
Antonio De Marco <antonio@nuvole.org>
2020-08-19 13:29:16 +0200

    EWPP-166: Rename post update config directory.

--- modules/oe_content_entity/modules/oe_content_entity_organisation/oe_content_entity_organisation.post_update.php
+++ modules/oe_content_entity/modules/oe_content_entity_organisation/oe_content_entity_organisation.post_update.php
@@ -10,10 +10,10 @@ declare(strict_types = 1);
 use Drupal\Core\Config\FileStorage;
 
 /**
- * Update configuration for oe_stakeholder.
+ * Create oe_stakeholder bundle.
  */
 function oe_content_entity_organisation_post_update_00001(): void {
   // Obtain configuration from yaml files.
-  $storage = new FileStorage(drupal_get_path('module', 'oe_content_entity_organisation') . '/config/post_updates/00001_oe_stakeholder');
+  $storage = new FileStorage(drupal_get_path('module', 'oe_content_entity_organisation') . '/config/post_updates/00001_create_stakeholder_bundle');
   \Drupal::service('config.installer')->installOptionalConfig($storage);
 }
