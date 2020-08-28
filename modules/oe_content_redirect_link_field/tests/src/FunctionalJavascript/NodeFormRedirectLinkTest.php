53cc69ea006957fafd57b2c8889d91e664da924c
upchuk <danny@webomelette.com>
2020-08-28 11:29:28 +0200

    EWPP-118: Consolidating test and updating comments and docs.

--- modules/oe_content_redirect_link_field/tests/src/FunctionalJavascript/NodeFormRedirectLinkTest.php
+++ modules/oe_content_redirect_link_field/tests/src/FunctionalJavascript/NodeFormRedirectLinkTest.php
@@ -11,7 +11,7 @@ use Drupal\node\Entity\NodeType;
 use Drupal\Tests\node\Traits\NodeCreationTrait;
 
 /**
- * Tests the redirect link field withing node add/edit form.
+ * Tests the redirect link field within the node add/edit form.
  */
 class NodeFormRedirectLinkTest extends WebDriverTestBase {
 
@@ -25,7 +25,7 @@ class NodeFormRedirectLinkTest extends WebDriverTestBase {
   /**
    * {@inheritdoc}
    */
-  public static $modules = [
+  protected static $modules = [
     'oe_content_redirect_link_field',
   ];
 
@@ -64,14 +64,15 @@ class NodeFormRedirectLinkTest extends WebDriverTestBase {
   /**
    * Test 'oe_redirect_link' behavior.
    */
-  public function testRedirectLinkSavings(): void {
+  public function testRedirectLinkForm(): void {
     FieldConfig::create([
       'entity_type' => 'node',
       'field_name' => 'oe_redirect_link',
       'bundle' => $this->nodeType->id(),
       'settings' => [
         'link_type' => LinkItemInterface::LINK_EXTERNAL,
-        'title' => 0,
+        // Enable the title field so we can test it doesn't show up.
+        'title' => 1,
       ],
     ])->save();
     /** @var \Drupal\Core\Entity\EntityDisplayRepositoryInterface $display_repository */
@@ -86,7 +87,7 @@ class NodeFormRedirectLinkTest extends WebDriverTestBase {
     $this->drupalLogin($this->nodeUser);
     $this->drupalGet('node/add/' . $this->nodeType->id());
 
-    // Make sure that checkbox for enabling redirect link is present.
+    // Make sure that checkbox for enabling the redirect link is present.
     $page = $this->getSession()->getPage();
     $redirect_link_details = $page->findAll('css', 'div#edit-advanced details#edit-redirect-link');
     $this->assertCount(1, $redirect_link_details);
@@ -97,11 +98,14 @@ class NodeFormRedirectLinkTest extends WebDriverTestBase {
     $redirect_link_field = $redirect_link_detail_block->find('css', 'div#edit-oe-redirect-link-wrapper');
     $this->assertFalse($redirect_link_field->isVisible());
 
-    // Make sure that 'redirect link' field is visible on checkbox click.
+    // Make sure that 'redirect link' field is visible on checking the box.
     $redirect_link_enable_checkbox->check();
     $this->assertTrue($redirect_link_field->isVisible());
     $this->assertTrue($redirect_link_field->hasField('oe_redirect_link[0][uri]'));
     $this->assertTrue($redirect_link_field->findField('oe_redirect_link[0][uri]')->isVisible());
+    // There should be no title element on the Link field regardless of
+    // configuration.
+    $this->assertEmpty($redirect_link_field->findField('oe_redirect_link[0][title]'));
     $this->assertEquals('This page will be redirected to this URL. All of its translations that do not have a language specific Redirect link URL filled in will also be redirected to this URL. Removing this value will prevent the redirect from happening on all of the translations as well.', $redirect_link_field->findById('edit-oe-redirect-link-0-uri--description')->getText());
 
     // Save node with empty 'redirect link' field.
@@ -110,9 +114,9 @@ class NodeFormRedirectLinkTest extends WebDriverTestBase {
     $this->assertSession()->pageTextContains('Test node with redirect link has been created.');
     $node = $this->drupalGetNodeByTitle('Test node with redirect link');
 
-    // Make sure that checkbox is unchecked
-    // if the 'redirect link' field is empty.
-    $this->drupalGet('node/' . $node->id() . '/edit');
+    // Make sure that checkbox is unchecked if the 'redirect link' field is
+    // empty.
+    $this->drupalGet($node->toUrl('edit-form'));
     $this->assertFalse($redirect_link_enable_checkbox->isChecked());
     $this->assertFalse($redirect_link_field->isVisible());
     $redirect_link_enable_checkbox->check();
@@ -120,15 +124,12 @@ class NodeFormRedirectLinkTest extends WebDriverTestBase {
     $this->assertEquals('', $uri_field->getValue());
 
     // Make sure that we accept only external links.
-    $uri_field->setValue('/node/999');
-    $page->pressButton('Save');
-    $this->assertSession()->pageTextNotContains('test Node type Test node with redirect link has been updated.');
     $uri_field->setValue('http://example.com');
     $page->pressButton('Save');
     $this->assertSession()->pageTextContains('Test node with redirect link has been updated.');
 
     // Make sure that the 'redirect link' field is saved.
-    $this->drupalGet('node/' . $node->id() . '/edit');
+    $this->drupalGet($node->toUrl('edit-form'));
     $this->assertTrue($redirect_link_enable_checkbox->isChecked());
     $this->assertTrue($redirect_link_enable_checkbox->isVisible());
     $this->assertEquals('http://example.com', $uri_field->getValue());
@@ -138,82 +139,10 @@ class NodeFormRedirectLinkTest extends WebDriverTestBase {
     $redirect_link_enable_checkbox->uncheck();
     $this->assertFalse($redirect_link_field->isVisible());
     $page->pressButton('Save');
-    $this->drupalGet('node/' . $node->id() . '/edit');
+    $this->drupalGet($node->toUrl('edit-form'));
     $this->assertFalse($redirect_link_enable_checkbox->isChecked());
     $this->assertFalse($redirect_link_field->isVisible());
     $this->assertEquals('', $uri_field->getValue());
-  }
-
-  /**
-   * Test behavior of redirect link field with different settings.
-   *
-   * @param array $settings
-   *   The array of field config settings.
-   *
-   * @dataProvider fieldSettingsDataProvider
-   */
-  public function testRedirectLinkWithDifferentSettings(array $settings): void {
-    FieldConfig::create([
-      'entity_type' => 'node',
-      'field_name' => 'oe_redirect_link',
-      'bundle' => $this->nodeType->id(),
-      'settings' => $settings,
-    ])->save();
-    /** @var \Drupal\Core\Entity\EntityDisplayRepositoryInterface $display_repository */
-    $display_repository = \Drupal::service('entity_display.repository');
-    $display_repository->getFormDisplay('node', $this->nodeType->id())
-      ->setComponent('oe_redirect_link', [
-        'type' => 'link_default',
-        'settings' => [],
-      ])
-      ->save();
-
-    $this->drupalLogin($this->nodeUser);
-    $this->drupalGet('node/add/' . $this->nodeType->id());
-
-    $page = $this->getSession()->getPage();
-    $redirect_link_details = $page->findAll('css', 'div#edit-advanced details#edit-redirect-link');
-    $this->assertCount(1, $redirect_link_details);
-    $redirect_link_detail_block = reset($redirect_link_details);
-    $redirect_link_field = $redirect_link_detail_block->find('css', 'div#edit-oe-redirect-link-wrapper');
-    $redirect_link_enable_checkbox = $redirect_link_detail_block->findField('Redirect this page to an external link');
-    $redirect_link_enable_checkbox->check();
-    $this->assertEquals(NULL, $redirect_link_field->findField('oe_redirect_link[0][title]'));
-    $uri_field = $redirect_link_field->findField('oe_redirect_link[0][uri]');
-    $uri_field->setValue('http://example.com');
-    $page->fillField('Title', 'Test node with redirect link');
-    $page->pressButton('Save');
-    $node = $this->drupalGetNodeByTitle('Test node with redirect link');
-    $this->drupalGet('node/' . $node->id() . '/edit');
-    $this->assertTrue($redirect_link_enable_checkbox->isChecked());
-    $this->assertTrue($redirect_link_enable_checkbox->isVisible());
-    $this->assertEquals('http://example.com', $uri_field->getValue());
-  }
-
-  /**
-   * Data provider for testRedirectLinkWithDifferentSettings().
-   */
-  public function fieldSettingsDataProvider(): array {
-    return [
-      [
-        'settings' => [
-          'link_type' => LinkItemInterface::LINK_EXTERNAL,
-          'title' => 0,
-        ],
-      ],
-      [
-        'settings' => [
-          'link_type' => LinkItemInterface::LINK_EXTERNAL,
-          'title' => 1,
-        ],
-      ],
-      [
-        'settings' => [
-          'link_type' => LinkItemInterface::LINK_EXTERNAL,
-          'title' => 2,
-        ],
-      ],
-    ];
   }
 
 }
