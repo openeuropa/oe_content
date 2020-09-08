824527aea4a7431207e19ed1b81a837390559bae
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2020-09-08 08:55:32 +0200

    EWPP-121: Implementation of redirect event subscriber.

---
+++ modules/oe_content_redirect_link_field/src/EventSubscriber/RedirectSubscriber.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_content_redirect_link_field\EventSubscriber;
+
+use Drupal\Component\Utility\UrlHelper;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Render\BubbleableMetadata;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+use Drupal\oe_content_redirect_link_field\RedirectLinkResolverInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * OpenEuropa Redirect Link Field event subscriber.
+ */
+class RedirectSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The redirect link retriever service.
+   *
+   * @var \Drupal\oe_content_redirect_link_field\RedirectLinkResolverInterface
+   */
+  protected $redirectLinkResolver;
+
+  /**
+   * Constructs event subscriber.
+   *
+   * @param \Drupal\oe_content_redirect_link_field\RedirectLinkResolverInterface $redirect_link_resolver
+   *   The redirect link resolver.
+   */
+  public function __construct(RedirectLinkResolverInterface $redirect_link_resolver) {
+    $this->redirectLinkResolver = $redirect_link_resolver;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      KernelEvents::REQUEST => ['onKernelRequest', 31],
+    ];
+  }
+
+  /**
+   * Handle request event for applying redirect if needed.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+   *   Response event.
+   */
+  public function onKernelRequest(GetResponseEvent $event): void {
+    $request = $event->getRequest();
+
+    $route = $request->attributes->get('_route');
+    if (empty($route)) {
+      return;
+    }
+
+    $route_parts = explode('.', $route);
+    if (count($route_parts) !== 3 || $route_parts[0] !== 'entity' || $route_parts[2] !== 'canonical') {
+      return;
+    }
+
+    $entity_type = $route_parts[1];
+    if (empty($request->attributes->get($entity_type))) {
+      return;
+    }
+
+    if (!$request->attributes->get($entity_type) instanceof ContentEntityInterface) {
+      return;
+    }
+
+    $entity = $request->attributes->get($entity_type);
+
+    $bubbleable_metadata = new BubbleableMetadata();
+    $bubbleable_metadata->addCacheContexts(['languages:language_interface']);
+    $redirect_path = $this->redirectLinkResolver->getPath($entity, $bubbleable_metadata);
+
+    if (!$redirect_path) {
+      return;
+    }
+
+    if (!UrlHelper::isExternal($redirect_path)) {
+      $redirect_path = $request->getUriForPath($redirect_path);
+    }
+
+    $redirect_response = new TrustedRedirectResponse($redirect_path, 301);
+    $redirect_response->addCacheableDependency($bubbleable_metadata);
+    $event->setResponse($redirect_response);
+  }
+
+}
