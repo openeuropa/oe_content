e8fdb1edad39869196558c0ef555f4f00a23cc12
upchuk <danny@webomelette.com>
2020-09-16 16:04:31 +0200

    EWPP-272: News type field with Skos concept subset.

--- modules/oe_content_news/oe_content_news.post_update.php
+++ modules/oe_content_news/oe_content_news.post_update.php
@@ -7,6 +7,8 @@
 
 declare(strict_types = 1);
 
+use Drupal\Core\Config\FileStorage;
+use Drupal\Core\Entity\Entity\EntityFormDisplay;
 use Drupal\field\Entity\FieldConfig;
 
 /**
@@ -21,5 +23,38 @@ function oe_content_news_post_update_00001_update_field_labels(array &$sandbox):
     $field_config = FieldConfig::load($id);
     $field_config->setLabel($new_label);
     $field_config->save();
+  }
+}
+
+/**
+ * Add type field.
+ */
+function oe_content_news_post_update_00002(): void {
+  $storage = new FileStorage(drupal_get_path('module', 'oe_content_news') . '/config/post_updates/00002');
+  $config_manager = \Drupal::service('config.manager');
+  $entity_manager = \Drupal::entityTypeManager();
+
+  // Create new field.
+  $config_ids = [
+    'field.storage.node.oe_news_types',
+    'field.field.node.oe_news.oe_news_types',
+  ];
+  foreach ($config_ids as $config_id) {
+    $config_record = $storage->read($config_id);
+    $entity_type = $config_manager->getEntityTypeIdByName($config_id);
+    $entity_storage = $entity_manager->getStorage($entity_type);
+    $entity = $entity_storage->createFromStorageRecord($config_record);
+    $entity->save();
+  }
+
+  // Update form display.
+  /** @var \Drupal\Core\Entity\Display\EntityFormDisplayInterface $form_display */
+  $values = $storage->read('core.entity_form_display.node.oe_news.default');
+  $display = EntityFormDisplay::load($values['id']);
+  if ($display) {
+    foreach ($values as $key => $value) {
+      $display->set($key, $value);
+    }
+    $display->save();
   }
 }
---
+++ modules/oe_content_news/src/Plugin/ConceptSubset/ResourceTypesNews.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_content_news\Plugin\ConceptSubset;
+
+use Drupal\Core\Entity\Query\QueryInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\rdf_entity\RdfFieldHandlerInterface;
+use Drupal\rdf_skos\ConceptSubsetPluginBase;
+use Drupal\rdf_skos\Plugin\PredicateMapperInterface;
+
+/**
+ * Resource type subset for news types.
+ *
+ * @ConceptSubset(
+ *   id = "oe_content_news_resource_types_news",
+ *   label = @Translation("News Resource Types"),
+ *   description = @Translation("Resource types to be used as the News type category."),
+ *   predicate_mapping = TRUE,
+ *   concept_schemes = {
+ *     "http://publications.europa.eu/resource/authority/resource-type"
+ *   }
+ * )
+ */
+class ResourceTypesNews extends ConceptSubsetPluginBase implements PredicateMapperInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterQuery(QueryInterface $query, $match_operator, array $concept_schemes = [], string $match = NULL): void {
+    $query->condition('oe_content_news_resource_types_news', 'http://publications.europa.eu/resource/authority/use-context/COM_NEWS');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPredicateMapping(): array {
+    $mapping = [];
+
+    $mapping['oe_content_news_resource_types_news'] = [
+      'column' => 'value',
+      'predicate' => ['http://lemon-model.net/lemon#context'],
+      'format' => RdfFieldHandlerInterface::RESOURCE,
+    ];
+
+    return $mapping;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getBaseFieldDefinitions(): array {
+    $fields = [];
+
+    $fields['oe_content_news_resource_types_news'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Resource type contexts'))
+      ->setDescription(t('Potential contexts of the resource type.'))
+      ->setCardinality(FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED);
+
+    return $fields;
+  }
+
+}
