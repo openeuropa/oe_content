9a5ad481aa8581f3e245a1950ef96fdaa8b47df5
Evgenii Nikitin <yenyasinn@gmail.com>
2020-09-17 18:57:16 +0300

    EWPP-270: Add "Details" field group to news content type.

--- modules/oe_content_news/oe_content_news.post_update.php
+++ modules/oe_content_news/oe_content_news.post_update.php
@@ -62,3 +62,28 @@ function oe_content_news_post_update_00003(): void {
     $display->save();
   }
 }
+
+/**
+ * Create oe_reference field in the news content type.
+ */
+function oe_content_news_post_update_00002(): void {
+  $storage = new FileStorage(drupal_get_path('module', 'oe_content_news') . '/config/post_updates/00002_create_fields');
+  \Drupal::service('config.installer')->installOptionalConfig($storage);
+}
+
+/**
+ * Update news node form display.
+ */
+function oe_content_news_post_update_00003(): void {
+  $storage = new FileStorage(drupal_get_path('module', 'oe_content_news') . '/config/post_updates/00003_update_form_display');
+
+  // Form display configurations to update.
+  $form_display_values = $storage->read('core.entity_form_display.node.oe_news.default');
+  $form_display = EntityFormDisplay::load($form_display_values['id']);
+  if ($form_display) {
+    $updated_form_display = \Drupal::entityTypeManager()
+      ->getStorage($form_display->getEntityTypeId())
+      ->updateFromStorageRecord($form_display, $form_display_values);
+    $updated_form_display->save();
+  }
+}
