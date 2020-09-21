b2ff5e2ba09d4d9d5dd5c9125621cf7c8d18d45c
nagyad <joevagyok@gmail.com>
2020-09-21 14:14:06 +0200

    EWPP-274: Fix title field name and create behat context for tender.

---
+++ tests/Behat/Content/Node/TenderContentContext.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_content\Behat\Content\Node;
+
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Drupal\Tests\oe_content\Behat\Hook\Scope\BeforeParseEntityFieldsScope;
+use Drupal\Tests\oe_content\Traits\EntityLoadingTrait;
+use Drupal\Tests\oe_content\Traits\EntityReferenceRevisionTrait;
+use Drupal\Tests\oe_content\Traits\EntityReferenceTrait;
+
+/**
+ * Context to create call for tender content entities.
+ *
+ * @SuppressWarnings(PHPMD)
+ */
+class TenderContentContext extends RawDrupalContext {
+
+  use EntityReferenceRevisionTrait;
+  use EntityReferenceTrait;
+  use EntityLoadingTrait;
+
+  /**
+   * Run before fields are parsed by Drupal Behat extension.
+   *
+   * @param \Drupal\Tests\oe_content\Behat\Hook\Scope\BeforeParseEntityFieldsScope $scope
+   *   Behat hook scope.
+   *
+   * @BeforeParseEntityFields(node,oe_project)
+   */
+  public function alterProjectFields(BeforeParseEntityFieldsScope $scope): void {
+    // Map human readable field names to their Behat parsable machine names.
+    $mapping = [
+      'Alternative title' => 'oe_content_short_title',
+      'Body text' => 'body',
+      'Responsible department' => 'oe_departments',
+      'Opening date' => 'oe_tender_opening_date',
+      'Deadline date' => 'oe_tender_deadline',
+      'Publication date' => 'oe_publication_date',
+      'Published' => 'status',
+      'Reference' => 'oe_reference_code',
+      'Documents' => 'oe_documents',
+      'Summary' => 'oe_summary',
+      'Teaser' => 'oe_teaser',
+      'Title' => 'title',
+    ];
+
+    foreach ($scope->getFields() as $key => $value) {
+      switch ($key) {
+        // Set SKOS Concept entity reference fields.
+        case 'Responsible department':
+          $fields = $this->getReferenceField($mapping[$key], 'skos_concept', $value);
+          $scope->addFields($fields)->removeField($key);
+          break;
+
+        // Set Media entity reference fields.
+        case 'Documents':
+          $fields = $this->getReferenceField($mapping[$key], 'media', $value);
+          $scope->addFields($fields)->removeField($key);
+          break;
+
+        // Set content published status.
+        case 'Published':
+          $scope->addFields([
+            $mapping[$key] => (int) ($value === 'Yes'),
+          ])->removeField($key);
+          break;
+
+        default:
+          if (isset($mapping[$key])) {
+            $scope->renameField($key, $mapping[$key]);
+          }
+      }
+    }
+
+    // Set default fields.
+    $scope->addFields([
+      'oe_subject' => 'http://data.europa.eu/uxp/10',
+      'oe_content_content_owner' => 'http://publications.europa.eu/resource/authority/corporate-body/AASM',
+    ]);
+  }
+
+}
