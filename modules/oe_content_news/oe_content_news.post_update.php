5527b5e92f97b6afe49b16b74e2b5804020ad979
Evgenii Nikitin <yenyasinn@gmail.com>
2020-09-23 10:04:25 +0300

    EWPP-270: Post update with news type.

--- modules/oe_content_news/oe_content_news.post_update.php
+++ modules/oe_content_news/oe_content_news.post_update.php
@@ -27,43 +27,6 @@ function oe_content_news_post_update_00001_update_field_labels(array &$sandbox):
 }
 
 /**
- * Create new field News types.
- */
-function oe_content_news_post_update_00002(): void {
-  $storage = new FileStorage(drupal_get_path('module', 'oe_content_news') . '/config/post_updates/00002');
-  $config_manager = \Drupal::service('config.manager');
-  $entity_manager = \Drupal::entityTypeManager();
-
-  $config_ids = [
-    'field.storage.node.oe_news_types',
-    'field.field.node.oe_news.oe_news_types',
-  ];
-  foreach ($config_ids as $config_id) {
-    $config_record = $storage->read($config_id);
-    $entity_type = $config_manager->getEntityTypeIdByName($config_id);
-    $entity_storage = $entity_manager->getStorage($entity_type);
-    $entity = $entity_storage->createFromStorageRecord($config_record);
-    $entity->save();
-  }
-}
-
-/**
- * Update form display.
- */
-function oe_content_news_post_update_00003(): void {
-  $storage = new FileStorage(drupal_get_path('module', 'oe_content_news') . '/config/post_updates/00003');
-  /** @var \Drupal\Core\Entity\Display\EntityFormDisplayInterface $form_display */
-  $values = $storage->read('core.entity_form_display.node.oe_news.default');
-  $display = EntityFormDisplay::load($values['id']);
-  if ($display) {
-    foreach ($values as $key => $value) {
-      $display->set($key, $value);
-    }
-    $display->save();
-  }
-}
-
-/**
  * Enable new modules from dependency.
  */
 function oe_content_news_post_update_00002(): void {
@@ -71,7 +34,7 @@ function oe_content_news_post_update_00002(): void {
 }
 
 /**
- * Create oe_reference field in the news content type.
+ * Create oe_reference, oe_news_types fields in the news content type.
  */
 function oe_content_news_post_update_00003(): void {
   $storage = new FileStorage(drupal_get_path('module', 'oe_content_news') . '/config/post_updates/00003_create_fields');
