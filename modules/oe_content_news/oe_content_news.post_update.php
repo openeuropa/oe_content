d43ae7a1e5d80615cc3036d05ebad4b335e87344
kaikiku <kaikiku@gmail.com>
2020-09-28 12:35:53 +0200

    D8TSDT-120: Reduce the number of hook_post_update calls.

--- modules/oe_content_news/oe_content_news.post_update.php
+++ modules/oe_content_news/oe_content_news.post_update.php
@@ -30,11 +30,20 @@ function oe_content_news_post_update_00001_update_field_labels(array &$sandbox):
  * Enable new modules from dependency.
  */
 function oe_content_news_post_update_00002(): void {
-  \Drupal::service('module_installer')->install(['field_group', 'oe_content_reference_code_field']);
+  $modules = [
+    'field_group',
+    'oe_content_reference_code_field',
+    'oe_content_entity_contact',
+    'composite_reference',
+    'entity_reference_revisions',
+  ];
+  \Drupal::service('module_installer')->install($modules);
 }
 
 /**
- * Create oe_reference, oe_news_types fields in the news content type.
+ * Create new fields in the news content type.
+ *
+ * Add fields oe_reference, oe_news_types and oe_news_contacts.
  */
 function oe_content_news_post_update_00003(): void {
   $storage = new FileStorage(drupal_get_path('module', 'oe_content_news') . '/config/post_updates/00003_create_fields');
@@ -46,31 +55,6 @@ function oe_content_news_post_update_00003(): void {
  */
 function oe_content_news_post_update_00004(): void {
   $storage = new FileStorage(drupal_get_path('module', 'oe_content_news') . '/config/post_updates/00004_update_form_display');
-
-  // Form display configurations to update.
-  $form_display_values = $storage->read('core.entity_form_display.node.oe_news.default');
-  $form_display = EntityFormDisplay::load($form_display_values['id']);
-  if ($form_display) {
-    $updated_form_display = \Drupal::entityTypeManager()
-      ->getStorage($form_display->getEntityTypeId())
-      ->updateFromStorageRecord($form_display, $form_display_values);
-    $updated_form_display->save();
-  }
-}
-
-/**
- * Create oe_news_contacts field in the news content type.
- */
-function oe_content_news_post_update_00005(): void {
-  $storage = new FileStorage(drupal_get_path('module', 'oe_content_news') . '/config/post_updates/00005_create_fields');
-  \Drupal::service('config.installer')->installOptionalConfig($storage);
-}
-
-/**
- * Update news node form display.
- */
-function oe_content_news_post_update_00006(): void {
-  $storage = new FileStorage(drupal_get_path('module', 'oe_content_news') . '/config/post_updates/00006_update_form_display');
 
   // Form display configurations to update.
   $form_display_values = $storage->read('core.entity_form_display.node.oe_news.default');
