1ee2293fae0f17d879a9f128b89652856e238e5f
George Baev <geobaev@gmail.com>
2020-11-05 07:23:08 +0200

    EWPP-436: Alter the Deadline validator.

--- modules/oe_content_call_proposals/src/Plugin/Validation/Constraint/CallProposalsDeadlineDateValidator.php
+++ modules/oe_content_call_proposals/src/Plugin/Validation/Constraint/CallProposalsDeadlineDateValidator.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Drupal\oe_content_call_proposals\Plugin\Validation\Constraint;
 
+use Drupal\oe_content_call_proposals\CallForProposalsNodeWrapper;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Drupal\oe_content_call_proposals\CallForProposalsNodeWrapperInterface;
@@ -17,14 +18,17 @@ class CallProposalsDeadlineDateValidator extends ConstraintValidator {
    * {@inheritdoc}
    */
   public function validate($node, Constraint $constraint) {
-    /* @var \Drupal\node\NodeInterface $node */
+    /** @var \Drupal\node\NodeInterface $node */
     if (!isset($node) || $node->getType() !== 'oe_call_proposals') {
       return;
     }
 
-    $deadline_model = $node->oe_call_proposals_model->value;
-    $deadline_date = $node->oe_call_proposals_deadline->value;
-    if ($deadline_model !== CallForProposalsNodeWrapperInterface::MODEL_PERMANENT && empty($deadline_date)) {
+    $wrapper = CallForProposalsNodeWrapper::getInstance($node);
+
+    $deadline_model = $wrapper->getModel();
+    $has_deadline_date = $wrapper->hasDeadlineDate();
+    if ($deadline_model !== CallForProposalsNodeWrapperInterface::MODEL_PERMANENT
+        && $has_deadline_date === FALSE) {
       $this->context->buildViolation($constraint->errorMessage)
         ->atPath('oe_call_proposals_deadline')
         ->addViolation();
