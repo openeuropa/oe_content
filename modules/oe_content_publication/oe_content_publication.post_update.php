02e18ba88b2dffffee74984cd00b91eaa97eee14
George Baev <geobaev@gmail.com>
2020-11-11 11:35:36 +0200

    EWPP-430: Alter the post_update functionality.

--- modules/oe_content_publication/oe_content_publication.post_update.php
+++ modules/oe_content_publication/oe_content_publication.post_update.php
@@ -34,21 +34,46 @@ function oe_content_publication_post_update_00002() {
 }
 
 /**
- * Create the body field in the Publication content type.
+ * Create the new fields in the Publication content type.
  */
 function oe_content_publication_post_update_00003(): void {
   $path = drupal_get_path('module', 'oe_content_publication');
 
-  $storage = new FileStorage($path . '/config/post_updates/00003_create_body_field');
+  $storage = new FileStorage($path . '/config/post_updates/00003_create_fields');
   \Drupal::service('config.installer')->installOptionalConfig($storage);
+}
+
+/**
+ * Update field labels.
+ */
+function oe_content_publication_post_update_00004(array &$sandbox): void {
+  $new_field_labels = [
+    'node.oe_publication.oe_publication_type' => 'Resource type',
+    'node.oe_publication.oe_documents' => 'Files',
+  ];
+  foreach ($new_field_labels as $id => $new_label) {
+    $field_config = FieldConfig::load($id);
+    $field_config->setLabel($new_label);
+    $field_config->save();
+  }
+
+  $new_translatable_settings = [
+    'node.oe_publication.oe_summary' => FALSE,
+    'node.oe_publication.oe_teaser' => FALSE,
+  ];
+  foreach ($new_translatable_settings as $id => $value) {
+    $field_config = FieldConfig::load($id);
+    $field_config->setTranslatable($value);
+    $field_config->save();
+  }
 }
 
 /**
  * Update Publication node form display.
  */
-function oe_content_publication_post_update_00004(): void {
+function oe_content_publication_post_update_00005(): void {
   $path = drupal_get_path('module', 'oe_content_publication');
-  $storage = new FileStorage($path . '/config/post_updates/00004_update_form_display');
+  $storage = new FileStorage($path . '/config/post_updates/00005_update_form_display');
 
   // Form display configurations to update.
   $form_display_values = $storage->read('core.entity_form_display.node.oe_publication.default');
@@ -64,9 +89,9 @@ function oe_content_publication_post_update_00004(): void {
 /**
  * Update Publication node view display.
  */
-function oe_content_publication_post_update_00005(): void {
+function oe_content_publication_post_update_00006(): void {
   $path = drupal_get_path('module', 'oe_content_publication');
-  $storage = new FileStorage($path . '/config/post_updates/00005_update_view_display');
+  $storage = new FileStorage($path . '/config/post_updates/00006_update_view_display');
 
   // View display configurations to update.
   $view_display_values = $storage->read('core.entity_view_display.node.oe_publication.default');
