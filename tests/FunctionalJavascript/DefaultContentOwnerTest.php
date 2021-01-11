504439ff3214b5415fef2c704242a530168ddad7
George Baev <geobaev@gmail.com>
2021-01-11 17:06:20 +0200

    EWPP-615: Add more tests.

--- tests/FunctionalJavascript/DefaultContentOwnerTest.php
+++ tests/FunctionalJavascript/DefaultContentOwnerTest.php
@@ -58,4 +58,48 @@ class DefaultContentOwnerTest extends WebDriverTestBase {
     $assert_session->fieldValueEquals('oe_content_content_owner[1][target_id]', 'Directorate-General for Budget (http://publications.europa.eu/resource/authority/corporate-body/BUDG)');
   }
 
+  /**
+   * No default content owner is set.
+   *
+   * Tests that no default content owner is set in the node
+   * form if not set as site information.
+   */
+  public function testUnsetDefaultContentOwnerValues() {
+    $this->container->get('config.factory')->getEditable('oe_corporate_site_info.settings')
+      ->set('content_owners', [])
+      ->save();
+
+    $user = $this->drupalCreateUser([
+      'access content',
+      'create oe_page content',
+    ]);
+
+    $this->drupalLogin($user);
+
+    $this->drupalGet('/node/add/oe_page');
+
+    $this->assertSession()->fieldValueEquals('oe_content_content_owner[0][target_id]', '');
+  }
+
+  /**
+   * Module "Corporate Site Information" not enabled.
+   *
+   * Tests that uninstalling the "Corporate Site Information" module
+   * has no effect on the node form.
+   */
+  public function testCorporateSiteInfoNotEnabled() {
+    $this->container->get('module_installer')->uninstall(['oe_corporate_site_info']);
+
+    $user = $this->drupalCreateUser([
+      'access content',
+      'create oe_page content',
+    ]);
+
+    $this->drupalLogin($user);
+
+    $this->drupalGet('/node/add/oe_page');
+
+    $this->assertSession()->fieldValueEquals('oe_content_content_owner[0][target_id]', '');
+  }
+
 }
