db41c9b8956096ebd83570c35e9a13ec637a41a3
George Baev <geobaev@gmail.com>
2021-01-12 12:31:01 +0200

    EWPP-615: Collect all tests into one test.

--- tests/FunctionalJavascript/DefaultContentOwnerTest.php
+++ tests/FunctionalJavascript/DefaultContentOwnerTest.php
@@ -30,19 +30,20 @@ class DefaultContentOwnerTest extends WebDriverTestBase {
     parent::setUp();
 
     $this->setUpSparql();
+  }
 
+  /**
+   * Tests the content owner values on new nodes.
+   */
+  public function testContentOwnerValues() {
+    // Set the default content owner values.
     $this->container->get('config.factory')->getEditable('oe_corporate_site_info.settings')
       ->set('content_owners', [
         'http://publications.europa.eu/resource/authority/corporate-body/AGRI',
         'http://publications.europa.eu/resource/authority/corporate-body/BUDG',
       ])
       ->save();
-  }
 
-  /**
-   * Tests the default content owner values on new nodes.
-   */
-  public function testDefaultContentOwnerValues() {
     $user = $this->drupalCreateUser([
       'access content',
       'create oe_page content',
@@ -54,52 +55,28 @@ class DefaultContentOwnerTest extends WebDriverTestBase {
     $this->drupalGet('/node/add/oe_page');
 
     $assert_session = $this->assertSession();
+
+    // Assert that default owner values are set when creating a new node.
     $assert_session->fieldValueEquals('oe_content_content_owner[0][target_id]', 'Directorate-General for Agriculture and Rural Development (http://publications.europa.eu/resource/authority/corporate-body/AGRI)');
     $assert_session->fieldValueEquals('oe_content_content_owner[1][target_id]', 'Directorate-General for Budget (http://publications.europa.eu/resource/authority/corporate-body/BUDG)');
-  }
 
-  /**
-   * No default content owner is set.
-   *
-   * Tests that no default content owner is set in the node
-   * form if not set as site information.
-   */
-  public function testUnsetDefaultContentOwnerValues() {
+    // Unset the default content owner values.
     $this->container->get('config.factory')->getEditable('oe_corporate_site_info.settings')
       ->set('content_owners', [])
       ->save();
 
-    $user = $this->drupalCreateUser([
-      'access content',
-      'create oe_page content',
-    ]);
-
-    $this->drupalLogin($user);
-
     $this->drupalGet('/node/add/oe_page');
 
-    $this->assertSession()->fieldValueEquals('oe_content_content_owner[0][target_id]', '');
-  }
+    // Assert that no default content owners are set when creating a new node.
+    $assert_session->fieldValueEquals('oe_content_content_owner[0][target_id]', '');
 
-  /**
-   * Module "Corporate Site Information" not enabled.
-   *
-   * Tests that uninstalling the "Corporate Site Information" module
-   * has no effect on the node form.
-   */
-  public function testCorporateSiteInfoNotEnabled() {
+    // Uninstall the "Corporate Site Information" module.
     $this->container->get('module_installer')->uninstall(['oe_corporate_site_info']);
 
-    $user = $this->drupalCreateUser([
-      'access content',
-      'create oe_page content',
-    ]);
-
-    $this->drupalLogin($user);
-
     $this->drupalGet('/node/add/oe_page');
 
-    $this->assertSession()->fieldValueEquals('oe_content_content_owner[0][target_id]', '');
+    // Assert that the Content Owner form fields are displayed.
+    $assert_session->fieldValueEquals('oe_content_content_owner[0][target_id]', '');
   }
 
 }
