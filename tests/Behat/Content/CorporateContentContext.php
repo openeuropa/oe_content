067bb1bc34c5a8a267c9627010de895ca12d42e3
Antonio De Marco <antonio@nuvole.org>
2021-02-10 18:02:37 +0100

    EWPP-667: Add RawEntityContext.

--- tests/Behat/Content/CorporateContentContext.php
+++ tests/Behat/Content/CorporateContentContext.php
@@ -5,29 +5,15 @@ declare(strict_types = 1);
 namespace Drupal\Tests\oe_content\Behat\Content;
 
 use Behat\Gherkin\Node\TableNode;
-use Behat\Testwork\Call\CallResults;
 use Drupal\Core\Entity\EntityInterface;
-use Drupal\DrupalExtension\Context\RawDrupalContext;
-use Drupal\Tests\oe_content\Behat\Hook\Scope\AfterParseEntityFieldsScope;
-use Drupal\Tests\oe_content\Behat\Hook\Scope\AfterSaveEntityScope;
-use Drupal\Tests\oe_content\Behat\Hook\Scope\BeforeParseEntityFieldsScope;
-use Drupal\Tests\oe_content\Behat\Hook\Scope\BeforeSaveEntityScope;
-use Drupal\Tests\oe_content\Behat\Hook\Scope\EntityAwareHookScopeInterface;
 use Drupal\Tests\oe_content\Traits\EntityLoadingTrait;
 
 /**
  * Context to create corporate entities.
  */
-class CorporateContentContext extends RawDrupalContext {
+class CorporateContentContext extends RawEntityContext {
 
   use EntityLoadingTrait;
-
-  /**
-   * Keep track of created corporate entities so they can be cleaned up.
-   *
-   * @var array
-   */
-  protected $entities = [];
 
   /**
    * Create an entity.
@@ -46,19 +32,6 @@ class CorporateContentContext extends RawDrupalContext {
    *
    * Use entity type and bundle labels to refer to the entity.
    *
-   * Field names and/or values can be transformed by using the following hooks:
-   *
-   *  - @BeforeParseEntityFields(ENTITY_TYPE, ENTITY_BUNDLE)
-   *  - @AfterParseEntityFields(ENTITY_TYPE, ENTITY_BUNDLE)
-   *
-   * For an example of field transformations refer to:
-   *
-   * - @see \Drupal\Tests\oe_content\Behat\Content\Node\EventContentContext::alterEventFields()
-   * - @see \Drupal\Tests\oe_content\Behat\Content\Venue\DefaultVenueContext::alterVenueFields()
-   *
-   * This step also fires a @BeforeSaveEntity(ENTITY_TYPE, ENTITY_BUNDLE) right
-   * before saving the entity.
-   *
    * @param string $bundle_label
    *   Entity bundle label.
    * @param string $entity_type_label
@@ -66,49 +39,23 @@ class CorporateContentContext extends RawDrupalContext {
    * @param \Behat\Gherkin\Node\TableNode $table
    *   List of fields.
    *
+   * @see \Drupal\Tests\oe_content\Behat\Content\RawEntityContext::saveEntity()
+   *
    * @Given the following :bundle_label :entity_type_label entity:
    */
   public function createEntity(string $bundle_label, string $entity_type_label, TableNode $table): void {
     $definition = $this->loadDefinitionByLabel($entity_type_label);
     $entity_type = $definition->id();
+    $bundle = $this->loadEntityByLabel($definition->getBundleEntityType(), $bundle_label)->id();
 
     // Get and alter fields.
     $fields = $table->getRowsHash();
-    $bundle = $this->loadEntityByLabel($definition->getBundleEntityType(), $bundle_label)->id();
     $fields[$definition->getKey('bundle')] = $bundle;
     $fields = $this->parseFields($entity_type, $bundle, $fields);
 
-    // Create entity.
+    // Create and save entity.
     $entity = \Drupal::entityTypeManager()->getStorage($entity_type)->create($fields);
-
-    // Dispatch before save hook.
-    $scope = new BeforeSaveEntityScope($entity_type, $bundle, $this->getDrupal()->getEnvironment(), $entity);
-    $this->dispatchEntityAwareHook($scope);
-
-    $entity->save();
-
-    // Dispatch after save hook.
-    $scope = new AfterSaveEntityScope($entity_type, $bundle, $this->getDrupal()->getEnvironment(), $entity);
-    $this->dispatchEntityAwareHook($scope);
-
-    // Make sure that the created entities are tracked and can be cleaned up.
-    $this->entities[] = $entity;
-
-    // Clears the static cache of DatabaseCacheTagsChecksum.
-    // Static caches are typically cleared at the end of the request since a
-    // typical web request is short lived and the process disappears when the
-    // page is delivered. But if a Behat test is using DrupalContext then Drupal
-    // will be bootstrapped early on (in the BeforeSuiteScope step). This starts
-    // a request which is not short lived, but can live for several minutes
-    // while the tests run. During the lifetime of this request there will be
-    // steps executed that do requests of their own, changing the state of the
-    // Drupal site. This does not however update any of the statically cached
-    // data of the parent request, so this is totally unaware of the changes.
-    // This causes unexpected behaviour like the failure to invalidate some
-    // caches because DatabaseCacheTagsChecksum::invalidateTags() keeps a local
-    // storage of which cache tags were invalidated, and this is not reset in
-    // time.
-    \Drupal::service('cache_tags.invalidator')->resetCheckSums();
+    $this->saveEntity($entity_type, $bundle, $entity);
   }
 
   /**
@@ -122,19 +69,6 @@ class CorporateContentContext extends RawDrupalContext {
    *
    * Use entity type and bundle labels to refer to the entity.
    *
-   * Field names and/or values can be transformed by using the following hooks:
-   *
-   *  - @BeforeParseEntityFields(ENTITY_TYPE, ENTITY_BUNDLE)
-   *  - @AfterParseEntityFields(ENTITY_TYPE, ENTITY_BUNDLE)
-   *
-   * For an example of field transformations refer to:
-   *
-   * - @see \Drupal\Tests\oe_content\Behat\Content\Node\EventContentContext::alterEventFields()
-   * - @see \Drupal\Tests\oe_content\Behat\Content\Venue\DefaultVenueContext::alterVenueFields()
-   *
-   * This step also fires a @BeforeSaveEntity(ENTITY_TYPE, ENTITY_BUNDLE) right
-   * before saving the entity.
-   *
    * @param string $bundle_label
    *   Entity bundle label.
    * @param string $entity_type_label
@@ -144,15 +78,17 @@ class CorporateContentContext extends RawDrupalContext {
    * @param \Behat\Gherkin\Node\TableNode $table
    *   List of fields.
    *
+   * @see \Drupal\Tests\oe_content\Behat\Content\RawEntityContext::saveEntity()
+   *
    * @Given the :bundle_label :entity_type_label :label is updated as follows:
    */
   public function updateEntity(string $bundle_label, string $entity_type_label, string $label, TableNode $table): void {
     $definition = $this->loadDefinitionByLabel($entity_type_label);
     $entity_type = $definition->id();
+    $bundle = $this->loadEntityByLabel($definition->getBundleEntityType(), $bundle_label)->id();
 
     // Get and alter fields.
     $fields = $table->getRowsHash();
-    $bundle = $this->loadEntityByLabel($definition->getBundleEntityType(), $bundle_label)->id();
     $fields = $this->parseFields($entity_type, $bundle, $fields);
 
     // Set field value and save the entity.
@@ -161,31 +97,8 @@ class CorporateContentContext extends RawDrupalContext {
       $entity->set($name, $value);
     }
 
-    // Dispatch before save hook.
-    $scope = new BeforeSaveEntityScope($entity_type, $bundle, $this->getDrupal()->getEnvironment(), $entity);
-    $this->dispatchEntityAwareHook($scope);
-
-    $entity->save();
-
-    // Dispatch after save hook.
-    $scope = new AfterSaveEntityScope($entity_type, $bundle, $this->getDrupal()->getEnvironment(), $entity);
-    $this->dispatchEntityAwareHook($scope);
-
-    // Clears the static cache of DatabaseCacheTagsChecksum.
-    // Static caches are typically cleared at the end of the request since a
-    // typical web request is short lived and the process disappears when the
-    // page is delivered. But if a Behat test is using DrupalContext then Drupal
-    // will be bootstrapped early on (in the BeforeSuiteScope step). This starts
-    // a request which is not short lived, but can live for several minutes
-    // while the tests run. During the lifetime of this request there will be
-    // steps executed that do requests of their own, changing the state of the
-    // Drupal site. This does not however update any of the statically cached
-    // data of the parent request, so this is totally unaware of the changes.
-    // This causes unexpected behaviour like the failure to invalidate some
-    // caches because DatabaseCacheTagsChecksum::invalidateTags() keeps a local
-    // storage of which cache tags were invalidated, and this is not reset in
-    // time.
-    \Drupal::service('cache_tags.invalidator')->resetCheckSums();
+    // Update entity.
+    $this->saveEntity($entity_type, $bundle, $entity);
   }
 
   /**
@@ -209,75 +122,6 @@ class CorporateContentContext extends RawDrupalContext {
     $entity_type = $definition->id();
     $bundle = $this->loadEntityByLabel($definition->getBundleEntityType(), $bundle_label)->id();
     $this->loadEntityByLabel($entity_type, $title, $bundle);
-  }
-
-  /**
-   * Remove any created corporate entities.
-   *
-   * @AfterScenario
-   */
-  public function cleanEntities(): void {
-    foreach ($this->entities as $entity) {
-      $this->getDriver()->getCore()->entityDelete($entity->id(), $entity);
-    }
-    $this->entities = [];
-  }
-
-  /**
-   * Parse entity fields.
-   *
-   * Also fires the following two Behat hooks:
-   *
-   * - BeforeParseEntityFieldsScope(entity_type,bundle)
-   * - AfterParseEntityFields(entity_type,bundle)
-   *
-   * @param string $entity_type
-   *   Entity type.
-   * @param string $bundle
-   *   Entity bundle.
-   * @param array $fields
-   *   Fields to be altered.
-   *
-   * @return array
-   *   Parsed fields.
-   */
-  protected function parseFields(string $entity_type, string $bundle, array $fields): array {
-    $scope = new BeforeParseEntityFieldsScope($entity_type, $bundle, $this->getDrupal()->getEnvironment(), $fields);
-    $this->dispatchEntityAwareHook($scope);
-
-    // We have to cast as parseEntityFields() expects a reference to an object.
-    $fields = (object) $scope->getFields();
-    $this->parseEntityFields($entity_type, $fields);
-
-    // We cast it back so we keep dealing with arrays.
-    $fields = (array) $fields;
-    $scope = new AfterParseEntityFieldsScope($entity_type, $bundle, $this->getDrupal()->getEnvironment(), $fields);
-    $this->dispatchEntityAwareHook($scope);
-
-    return $scope->getFields();
-  }
-
-  /**
-   * Dispatch entity aware hooks.
-   *
-   * @param \Drupal\Tests\oe_content\Behat\Hook\Scope\EntityAwareHookScopeInterface $scope
-   *   Hook scope to dispatch.
-   *
-   * @return \Behat\Testwork\Call\CallResults
-   *   Results of hook dispatch.
-   */
-  protected function dispatchEntityAwareHook(EntityAwareHookScopeInterface $scope): CallResults {
-    $results = $this->dispatcher->dispatchScopeHooks($scope);
-
-    foreach ($results as $result) {
-      // The dispatcher suppresses exceptions, throw them here if there are any.
-      if ($result->hasException()) {
-        $exception = $result->getException();
-        throw $exception;
-      }
-    }
-
-    return $results;
   }
 
   /**
@@ -333,18 +177,8 @@ class CorporateContentContext extends RawDrupalContext {
       $entity->set($name, $value);
     }
 
-    // Dispatch before save hook.
-    $scope = new BeforeSaveEntityScope($entity_type, $bundle, $this->getDrupal()->getEnvironment(), $entity);
-    $this->dispatchEntityAwareHook($scope);
-
-    $entity->save();
-
-    // Dispatch after save hook.
-    $scope = new AfterSaveEntityScope($entity_type, $bundle, $this->getDrupal()->getEnvironment(), $entity);
-    $this->dispatchEntityAwareHook($scope);
-
-    // Clears the static cache of DatabaseCacheTagsChecksum.
-    \Drupal::service('cache_tags.invalidator')->resetCheckSums();
+    // Update entity.
+    $this->saveEntity($entity_type, $bundle, $entity);
   }
 
   /**
---
+++ tests/Behat/Content/RawEntityContext.php
@@ -0,0 +1,166 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_content\Behat\Content;
+
+use Behat\Testwork\Call\CallResults;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Drupal\Tests\oe_content\Behat\Hook\Scope\AfterParseEntityFieldsScope;
+use Drupal\Tests\oe_content\Behat\Hook\Scope\AfterSaveEntityScope;
+use Drupal\Tests\oe_content\Behat\Hook\Scope\BeforeParseEntityFieldsScope;
+use Drupal\Tests\oe_content\Behat\Hook\Scope\BeforeSaveEntityScope;
+use Drupal\Tests\oe_content\Behat\Hook\Scope\EntityAwareHookScopeInterface;
+
+/**
+ * Enhance raw Drupal context with extra entity-related Behat hooks.
+ *
+ * Field names and/or values can be transformed by using the following hooks:
+ *
+ *  - @BeforeParseEntityFields(ENTITY_TYPE, ENTITY_BUNDLE)
+ *  - @AfterParseEntityFields(ENTITY_TYPE, ENTITY_BUNDLE)
+ *
+ * For an example of field transformations refer to:
+ *
+ * - @see \Drupal\Tests\oe_content\Behat\Content\Node\EventContentContext::alterEventFields()
+ * - @see \Drupal\Tests\oe_content\Behat\Content\Venue\DefaultVenueContext::alterVenueFields()
+ *
+ * This step also fires a @BeforeSaveEntity(ENTITY_TYPE, ENTITY_BUNDLE) right
+ * before saving the entity.
+ */
+class RawEntityContext extends RawDrupalContext {
+
+  /**
+   * Keep track of created corporate entities so they can be cleaned up.
+   *
+   * @var array
+   */
+  protected $entities = [];
+
+  /**
+   * Remove any created corporate entities.
+   *
+   * @AfterScenario
+   */
+  public function cleanEntities(): void {
+    foreach ($this->entities as $entity) {
+      $this->getDriver()->getCore()->entityDelete($entity->id(), $entity);
+    }
+    $this->entities = [];
+  }
+
+  /**
+   * Parse entity fields.
+   *
+   * Also fires the following two Behat hooks:
+   *
+   * - BeforeParseEntityFieldsScope(entity_type,bundle)
+   * - AfterParseEntityFields(entity_type,bundle)
+   *
+   * @param string $entity_type
+   *   Entity type.
+   * @param string $bundle
+   *   Entity bundle.
+   * @param array $fields
+   *   Fields to be altered.
+   *
+   * @return array
+   *   Parsed fields.
+   */
+  protected function parseFields(string $entity_type, string $bundle, array $fields): array {
+    $scope = new BeforeParseEntityFieldsScope($entity_type, $bundle, $this->getDrupal()->getEnvironment(), $fields);
+    $this->dispatchEntityAwareHook($scope);
+
+    // We have to cast as parseEntityFields() expects a reference to an object.
+    $fields = (object) $scope->getFields();
+    $this->parseEntityFields($entity_type, $fields);
+
+    // We cast it back so we keep dealing with arrays.
+    $fields = (array) $fields;
+    $scope = new AfterParseEntityFieldsScope($entity_type, $bundle, $this->getDrupal()->getEnvironment(), $fields);
+    $this->dispatchEntityAwareHook($scope);
+
+    return $scope->getFields();
+  }
+
+  /**
+   * Save given entity object and dispatch related Behat hooks.
+   *
+   * Field names and/or values can be transformed by using the following hooks:
+   *
+   *  - @BeforeParseEntityFields(ENTITY_TYPE, ENTITY_BUNDLE)
+   *  - @AfterParseEntityFields(ENTITY_TYPE, ENTITY_BUNDLE)
+   *
+   * For an example of field transformations refer to:
+   *
+   * - @see \Drupal\Tests\oe_content\Behat\Content\Node\EventContentContext::alterEventFields()
+   * - @see \Drupal\Tests\oe_content\Behat\Content\Venue\DefaultVenueContext::alterVenueFields()
+   *
+   * This step also fires a @BeforeSaveEntity(ENTITY_TYPE, ENTITY_BUNDLE) right
+   * before saving the entity.
+   *
+   * @param string $entity_type
+   *   Entity type machine name.
+   * @param string $bundle
+   *   Bundle machine name.
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   Entity object to be saved.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  protected function saveEntity(string $entity_type, string $bundle, EntityInterface $entity) {
+    // Dispatch before save hook.
+    $scope = new BeforeSaveEntityScope($entity_type, $bundle, $this->getDrupal()->getEnvironment(), $entity);
+    $this->dispatchEntityAwareHook($scope);
+
+    $entity->save();
+
+    // Dispatch after save hook.
+    $scope = new AfterSaveEntityScope($entity_type, $bundle, $this->getDrupal()->getEnvironment(), $entity);
+    $this->dispatchEntityAwareHook($scope);
+
+    // Make sure that the created entities are tracked and can be cleaned up.
+    $this->entities[] = $entity;
+
+    // Clears the static cache of DatabaseCacheTagsChecksum.
+    // Static caches are typically cleared at the end of the request since a
+    // typical web request is short lived and the process disappears when the
+    // page is delivered. But if a Behat test is using DrupalContext then Drupal
+    // will be bootstrapped early on (in the BeforeSuiteScope step). This starts
+    // a request which is not short lived, but can live for several minutes
+    // while the tests run. During the lifetime of this request there will be
+    // steps executed that do requests of their own, changing the state of the
+    // Drupal site. This does not however update any of the statically cached
+    // data of the parent request, so this is totally unaware of the changes.
+    // This causes unexpected behaviour like the failure to invalidate some
+    // caches because DatabaseCacheTagsChecksum::invalidateTags() keeps a local
+    // storage of which cache tags were invalidated, and this is not reset in
+    // time.
+    \Drupal::service('cache_tags.invalidator')->resetCheckSums();
+  }
+
+  /**
+   * Dispatch entity aware hooks.
+   *
+   * @param \Drupal\Tests\oe_content\Behat\Hook\Scope\EntityAwareHookScopeInterface $scope
+   *   Hook scope to dispatch.
+   *
+   * @return \Behat\Testwork\Call\CallResults
+   *   Results of hook dispatch.
+   */
+  protected function dispatchEntityAwareHook(EntityAwareHookScopeInterface $scope): CallResults {
+    $results = $this->dispatcher->dispatchScopeHooks($scope);
+
+    foreach ($results as $result) {
+      // The dispatcher suppresses exceptions, throw them here if there are any.
+      if ($result->hasException()) {
+        $exception = $result->getException();
+        throw $exception;
+      }
+    }
+
+    return $results;
+  }
+
+}
