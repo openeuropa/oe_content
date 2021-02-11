975d7a4b4c18c842a1ca41e64369a21b4d8688e5
Antonio De Marco <antonio@nuvole.org>
2021-02-11 11:31:26 +0100

    EWPP-667: Remove content storage and complete refactorying.

--- tests/Behat/Content/Node/ConsultationContentContext.php
+++ tests/Behat/Content/Node/ConsultationContentContext.php
@@ -8,7 +8,7 @@ use Drupal\Component\Datetime\DateTimePlus;
 use Drupal\Core\Datetime\DrupalDateTime;
 use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
 use Drupal\DrupalExtension\Context\RawDrupalContext;
-use Drupal\Tests\oe_content\Behat\Content\ContentStorage;
+use Drupal\Tests\oe_content\Behat\Content\Traits\GatherSubEntityContextTrait;
 use Drupal\Tests\oe_content\Behat\Hook\Scope\BeforeParseEntityFieldsScope;
 use Drupal\Tests\oe_content\Traits\EntityLoadingTrait;
 use Drupal\Tests\oe_content\Traits\EntityReferenceRevisionTrait;
@@ -24,6 +24,7 @@ class ConsultationContentContext extends RawDrupalContext {
   use EntityReferenceRevisionTrait;
   use EntityReferenceTrait;
   use EntityLoadingTrait;
+  use GatherSubEntityContextTrait;
 
   /**
    * Run before fields are parsed by Drupal Behat extension.
@@ -72,12 +73,11 @@ class ConsultationContentContext extends RawDrupalContext {
 
         // Set Document reference entity reference fields.
         case 'Documents':
-          $content_storage = ContentStorage::getInstance();
           $ids = [];
           $revision_ids = [];
           $names = explode(', ', $value);
           foreach ($names as $name) {
-            $entity = $content_storage->getEntity($name);
+            $entity = $this->subEntityContext->getSubEntityByName($name);
             $ids[] = $entity->id();
             $revision_ids[] = $entity->getRevisionId();
           }
--- tests/Behat/Content/SubEntityContext.php
+++ tests/Behat/Content/SubEntityContext.php
@@ -5,7 +5,6 @@ declare(strict_types = 1);
 namespace Drupal\Tests\oe_content\Behat\Content;
 
 use Behat\Gherkin\Node\TableNode;
-use Drupal\Core\Entity\EntityInterface;
 use Drupal\oe_content_sub_entity\Entity\SubEntityInterface;
 use Drupal\Tests\oe_content\Traits\EntityLoadingTrait;
 
@@ -24,13 +23,16 @@ class SubEntityContext extends RawEntityContext {
   protected $subEntities = [];
 
   /**
-   * Update an existing sub entity, given its bundle, entity type and title.
+   * Create a sub-entity by assigning an "Behat name" to it.
+   *
+   * Assigning a name to the sub-entity is necessary to use it when referencing
+   * it in nodes or when updating it. with the related step below.
    *
    * Example:
    *
-   * Given the sub entity Document "Document reference" "Document reference to
-   * My Document" is updated as follows:
-   *   | Published | No |
+   * Given the following "Document reference" "Document" sub-entity:
+   *   | Name     | My doc      |
+   *   | Document | example.doc |
    *
    * Use entity type and bundle labels to refer to the entity.
    *
@@ -38,31 +40,88 @@ class SubEntityContext extends RawEntityContext {
    *   Entity bundle label.
    * @param string $entity_type_label
    *   Entity type label.
-   * @param string $label
+   * @param \Behat\Gherkin\Node\TableNode $table
+   *   List of fields.
+   *
+   * @see \Drupal\Tests\oe_content\Behat\Content\RawEntityContext::saveEntity()
+   *
+   * @Given the following :bundle_label :entity_type_label sub-entity:
+   */
+  public function createSubEntity(string $bundle_label, string $entity_type_label, TableNode $table): void {
+    $fields = $table->getRowsHash();
+    if (!isset($fields['Name'])) {
+      throw new \InvalidArgumentException('You must specify a "Name" when creating a sub-entity.');
+    }
+
+    // Lead entity type definition by its label.
+    $definition = $this->loadDefinitionByLabel($entity_type_label);
+    $bundle = $this->loadEntityByLabel($definition->getBundleEntityType(), $bundle_label)->id();
+
+    // Create and save entity.
+    $entity = $this->createEntity($definition->id(), $bundle, $table->getRowsHash() + [
+      $definition->getKey('bundle') => $bundle,
+    ]);
+
+    $this->setSubEntityByName($fields['Name'], $entity);
+  }
+
+  /**
+   * Update an existing sub-entity, given its entity type and "Behat name".
+   *
+   * Example:
+   *
+   * Given the "Document reference" sub-entity "My doc" is updated as follows:
+   *   | Published | No |
+   *
+   * Note: the sub-entity must be created via the related creation step, before
+   * being updated. This is necessary since sub-entities do not have labels.
+   *
+   * @param string $entity_type_label
+   *   Entity type label.
+   * @param string $name
    *   Entity label.
    * @param \Behat\Gherkin\Node\TableNode $table
    *   List of fields.
    *
    * @see \Drupal\Tests\oe_content\Behat\Content\RawEntityContext::saveEntity()
    *
-   * @Given the sub entity :bundle_label :entity_type_label :name is updated as follows:
+   * @Given the :entity_type_label sub-entity :name is updated as follows:
    */
-  public function updateSubEntity(string $bundle_label, string $entity_type_label, string $label, TableNode $table): void {
+  public function updateSubEntity(string $entity_type_label, string $name, TableNode $table): void {
     $definition = $this->loadDefinitionByLabel($entity_type_label);
     $entity_type = $definition->id();
 
     // Get and alter fields.
     $fields = $table->getRowsHash();
-    if (!isset($fields['Name'])) {
-      $fields['Name'] = $label;
-    }
-    $bundle = $this->loadEntityByLabel($definition->getBundleEntityType(), $bundle_label)->id();
-
-    // Set field value and save the entity.
-    $entity = $this->loadSubEntityByName($entity_type, $label, $bundle);
+    $entity = $this->getSubEntityByName($name);
+    /** @var \Drupal\Core\Entity\ContentEntityInterface $entity */
+    $entity = \Drupal::entityTypeManager()->getStorage($entity_type)->load($entity->id());
 
     // Update entity.
     $this->updateEntity($entity, $fields);
+  }
+
+  /**
+   * Assert the sub entity exists.
+   *
+   * Example:
+   *
+   * Then the "Document reference" sub-entity "My doc" exists
+   *
+   * @param string $entity_type_label
+   *   Entity type label.
+   * @param string $name
+   *   Fake entity label.
+   *
+   * @Then the :entity_type_label sub-entity :name exists
+   */
+  public function subEntityExists(string $entity_type_label, string $name): void {
+    $entity_type = $this->loadDefinitionByLabel($entity_type_label)->id();
+    $entity = $this->getSubEntityByName($name);
+    $entity = \Drupal::entityTypeManager()->getStorage($entity_type)->load($entity->id());
+    if ($entity === NULL) {
+      throw new \Exception("The sub-entity with name '{$name}' does not exists.");
+    }
   }
 
   /**
@@ -101,70 +160,6 @@ class SubEntityContext extends RawEntityContext {
     }
 
     return $this->subEntities[$name];
-  }
-
-  /**
-   * Load a sub entity by type, label and, optionally, by bundle.
-   *
-   * @param string $entity_type
-   *   Entity type ID.
-   * @param string $label
-   *   Entity label.
-   * @param string $bundle
-   *   Entity bundle ID.
-   *
-   * @return \Drupal\Core\Entity\EntityInterface
-   *   Entity object, if any.
-   */
-  protected function loadSubEntityByName(string $entity_type, string $label, string $bundle): EntityInterface {
-    $exception_message = "No '$entity_type' entity of type '$bundle' with label '$label' has been found.";
-    $storage = \Drupal::entityTypeManager()->getStorage($entity_type);
-    // Find entity in the content storage.
-    $entity = ContentStorage::getInstance()->getEntity($label);
-    if (!$entity) {
-      throw new \InvalidArgumentException($exception_message);
-    }
-    $properties = [
-      'id' => $entity->id(),
-    ];
-
-    // If bundle is set then add it to the query properties.
-    if ($bundle) {
-      $bundle_key = $storage->getEntityType()->getKey('bundle');
-      $properties[$bundle_key] = $bundle;
-    }
-    // Load the sub entity from the database.
-    $entities = $storage->loadByProperties($properties);
-
-    if (empty($entities)) {
-      throw new \InvalidArgumentException($exception_message);
-    }
-
-    return reset($entities);
-  }
-
-  /**
-   * Assert the sub entity exists.
-   *
-   * Example:
-   *
-   * Then the sub entity Document "Document reference" entity with name
-   * "Document reference to My Document" exists
-   *
-   * @param string $bundle_label
-   *   Entity bundle label.
-   * @param string $entity_type_label
-   *   Entity type label.
-   * @param string $name
-   *   Fake entity label.
-   *
-   * @Then the sub entity :bundle_label :entity_type_label entity with name :name exists
-   */
-  public function subEntityExists(string $bundle_label, string $entity_type_label, string $name): void {
-    $definition = $this->loadDefinitionByLabel($entity_type_label);
-    $entity_type = $definition->id();
-    $bundle = $this->loadEntityByLabel($definition->getBundleEntityType(), $bundle_label)->id();
-    $this->loadSubEntityByName($entity_type, $name, $bundle);
   }
 
 }
