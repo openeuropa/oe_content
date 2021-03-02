9dd9e33e30a812dc6edc16cc9349f10f35ee413c
Imanol Eguskiza <imanoleguskizarojo@gmail.com>
2021-03-02 14:04:01 +0100

    EWPP-684: Add visible states values to fields that need it.

--- modules/oe_content_person/src/PersonNodeWrapper.php
+++ modules/oe_content_person/src/PersonNodeWrapper.php
@@ -11,6 +11,23 @@ use Drupal\oe_content\EntityWrapperBase;
  */
 class PersonNodeWrapper extends EntityWrapperBase {
 
+  const EU_ONLY_FIELDS = [
+    'oe_departments',
+    'oe_person_media',
+    'oe_social_media_links',
+    'oe_person_transparency_intro',
+    'oe_person_transparency_links',
+    'oe_person_biography_intro',
+    'oe_person_biography_timeline',
+    'oe_person_cv',
+    'oe_person_interests_intro',
+    'oe_person_interests_file',
+  ];
+
+  const NON_EU_ONLY_FIELDS = [
+    'oe_person_organisation',
+  ];
+
   /**
    * {@inheritdoc}
    */
