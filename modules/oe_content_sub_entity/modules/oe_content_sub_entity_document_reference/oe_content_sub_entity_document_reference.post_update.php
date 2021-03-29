dceb7e885a8cdb7938b06d6f503e9040ca16f09d
Imanol Eguskiza <imanoleguskizarojo@gmail.com>
2021-03-29 16:47:40 +0200

    EWPP-697: Update document reference entitites to hide untranslatable fields.

---
+++ modules/oe_content_sub_entity/modules/oe_content_sub_entity_document_reference/oe_content_sub_entity_document_reference.post_update.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file
+ * Post update functions for Content Sub Entity Document reference module.
+ */
+
+declare(strict_types = 1);
+
+use Drupal\Core\Config\FileStorage;
+
+/**
+ * Hide untranslatable fields for all bundles.
+ */
+function oe_content_sub_entity_document_reference_post_update_00001() {
+  $file_storage = new FileStorage(drupal_get_path('module', 'oe_content_sub_entity_document_reference') . '/config/post_updates/00001_hide_untranslatable_fields');
+  $storage = \Drupal::entityTypeManager()->getStorage('language_content_settings');
+
+  $config_ids = [
+    'language.content_settings.oe_document_reference.oe_document',
+    'language.content_settings.oe_document_reference.oe_publication',
+  ];
+  foreach ($config_ids as $config_id) {
+    $values = $file_storage->read($config_id);
+    $language_content_settings = $storage->load($values['id']);
+    if ($language_content_settings) {
+      $storage->updateFromStorageRecord($language_content_settings, $values);
+      $language_content_settings->save();
+    }
+  }
+
+}
