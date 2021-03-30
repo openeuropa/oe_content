2ccf21de8db9a32a6d25c36edb641655034571dd
Imanol Eguskiza <imanoleguskizarojo@gmail.com>
2021-03-30 08:52:55 +0200

    EWPP-697: Adjust label method for person job.

--- modules/oe_content_person/src/Entity/PersonJob.php
+++ modules/oe_content_person/src/Entity/PersonJob.php
@@ -59,11 +59,10 @@ class PersonJob extends SubEntityBase implements PersonJobInterface {
   public function label() {
     if ($this->bundle() === 'oe_default') {
       // Define label for Default Person job.
-      $entity_repository = \Drupal::service('entity.repository');
       $label = $this->get('oe_role_name')->value;
       if (!$this->get('oe_role_reference')->isEmpty()) {
         $vocabulary = $this->get('oe_role_reference')->entity;
-        $label = $entity_repository->getTranslationFromContext($vocabulary)->label();
+        $label = \Drupal::service('entity.repository')->getTranslationFromContext($vocabulary)->label();
         if ($this->get('oe_acting')->value) {
           $label = $this->t('(Acting) @role', ['@role' => $label]);
         }
