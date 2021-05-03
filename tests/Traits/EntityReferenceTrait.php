141809f73007e48c6e070eddb63b6d858ed4ec22
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2021-05-03 20:25:15 +0300

    EWPP-879: Rework approach for querying target entities.

--- tests/Traits/EntityReferenceTrait.php
+++ tests/Traits/EntityReferenceTrait.php
@@ -30,13 +30,36 @@ trait EntityReferenceTrait {
     $field_config = FieldConfig::loadByName($entity_type, $bundle, $field_name);
     /** @var \Drupal\Core\Entity\EntityReferenceSelection\SelectionInterface $handler */
     $handler = \Drupal::service('plugin.manager.entity_reference_selection')->getSelectionHandler($field_config);
+    $configuration = $handler->getConfiguration();
+    $entity_type_manager = \Drupal::entityTypeManager();
+    $target_entity_type = $entity_type_manager->getDefinition($configuration['target_type']);
+    $query = $entity_type_manager->getStorage($target_entity_type->id())->getQuery();
+
+    if ($target_entity_type->id() === 'skos_concept') {
+      $concept_schemes = $configuration['concept_schemes'];
+      if (!empty($concept_schemes)) {
+        $group = $query->orConditionGroup()
+          ->condition('in_scheme', $concept_schemes, 'IN')
+          ->condition('top_concept_of', $concept_schemes, 'IN');
+        $query->condition($group);
+      }
+    }
+    else {
+      if (!empty($configuration['target_bundles']) && is_array($configuration['target_bundles'])) {
+        $query->condition($target_entity_type->getKey('bundle'), $configuration['target_bundles'], 'IN');
+      }
+    }
     // Transform titles to ids and maintain the comma separated format.
     $items = explode(',', $labels);
     $items = array_map('trim', $items);
     $ids = [];
     foreach ($items as $item) {
-      $found_entities = $handler->getReferenceableEntities($item);
-      $ids[] = key(reset($found_entities));
+      if ($label_key = $target_entity_type->getKey('label')) {
+        $query_instance = clone $query;
+        $query_instance->condition($label_key, $item, 'CONTAINS');
+        $results = $query->execute();
+        $ids[] = reset($results);
+      }
     }
 
     return [
