68a6b42245fc94af84401259de54d7f9dc678e6d
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2021-08-25 15:26:44 +0300

    EWPP-1414: Improve tests coverage.

--- tests/src/FunctionalJavascript/DefaultContentOwnerTest.php
+++ tests/src/FunctionalJavascript/DefaultContentOwnerTest.php
@@ -99,6 +99,7 @@ class DefaultContentOwnerTest extends WebDriverTestBase {
     $user = $this->drupalCreateUser([
       'access content',
       'create oe_page content',
+      'edit any oe_page content',
       'view published skos concept entities',
     ]);
 
@@ -114,6 +115,15 @@ class DefaultContentOwnerTest extends WebDriverTestBase {
     $this->assertSession()->assertWaitOnAjaxRequest();
     $assert_session->fieldValueEquals('oe_authors[form][inline_entity_form][entities][0][form][oe_skos_reference][0][target_id]', 'Directorate-General for Agriculture and Rural Development (http://publications.europa.eu/resource/authority/corporate-body/AGRI)');
     $assert_session->fieldValueEquals('oe_authors[form][inline_entity_form][entities][0][form][oe_skos_reference][1][target_id]', 'Directorate-General for Budget (http://publications.europa.eu/resource/authority/corporate-body/BUDG)');
+    $this->getSession()->getPage()->fillField('oe_authors[form][inline_entity_form][entities][0][form][oe_skos_reference][1][target_id]', 'Arab Common Market');
+    $this->getSession()->getPage()->fillField('Page title', 'Title');
+    $this->getSession()->getPage()->fillField('Teaser', 'Teaser');
+    $this->getSession()->getPage()->pressButton('Save');
+    $this->drupalGet('/node/1/edit');
+    $this->getSession()->getPage()->pressButton('ief-oe_authors-form-entity-edit-0');
+    $this->assertSession()->assertWaitOnAjaxRequest();
+    $assert_session->fieldValueEquals('oe_authors[form][inline_entity_form][entities][0][form][oe_skos_reference][0][target_id]', 'Directorate-General for Agriculture and Rural Development (http://publications.europa.eu/resource/authority/corporate-body/AGRI)');
+    $assert_session->fieldValueEquals('oe_authors[form][inline_entity_form][entities][0][form][oe_skos_reference][1][target_id]', 'Arab Common Market (http://publications.europa.eu/resource/authority/corporate-body/ACM)');
 
     // Unset the default content owner values.
     $this->container->get('config.factory')->getEditable('oe_corporate_site_info.settings')
