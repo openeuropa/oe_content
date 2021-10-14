5a79e86baedcf69eb748645ee0b3096aea8e1d99
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2021-10-14 12:18:08 +0300

    EWPP-1410: Remove related to Author v2 upgrade path.

--- oe_content.post_update.php
+++ oe_content.post_update.php
@@ -7,7 +7,6 @@
 
 declare(strict_types = 1);
 
-use Drupal\Core\Config\FileStorage;
 use Drupal\Core\Utility\UpdateException;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\user\Entity\Role;
@@ -128,52 +127,5 @@ function oe_content_post_update_00004(): void {
     if ($role_changed) {
       $role->save();
     }
-  }
-}
-
-/**
- * Add new Author sub entity type with required fields.
- */
-function oe_content_post_update_30005(): void {
-  $entity_type_manager = \Drupal::entityTypeManager();
-  // Install new 'Author' and 'Author type' entity types.
-  $update_manager = \Drupal::entityDefinitionUpdateManager();
-  $update_manager->installEntityType($entity_type_manager->getDefinition('oe_author'));
-  $update_manager->installEntityType($entity_type_manager->getDefinition('oe_author_type'));
-
-  $file_storage = new FileStorage(drupal_get_path('module', 'oe_content') . '/config/post_updates/30005_create_oe_author_entity_type');
-
-  // Create Author bundle.
-  $entity_type_manager->getStorage('oe_author_type')
-    ->create(_oe_content_config_import_prepare($file_storage->read('oe_content.oe_author_type.oe_corporate_body')))
-    ->save();
-
-  // Create field for Author entity type.
-  $field_storage_config = $entity_type_manager->getStorage('field_storage_config');
-  $field_storage_config->create(_oe_content_config_import_prepare($file_storage->read('field.storage.oe_author.oe_skos_reference')))->save();
-  $field_config = $entity_type_manager->getStorage('field_config');
-  $field_config->create(_oe_content_config_import_prepare($file_storage->read('field.field.oe_author.oe_corporate_body.oe_skos_reference')))->save();
-
-  // Configure entity form display.
-  $entity_type_manager->getStorage('entity_form_display')->createFromStorageRecord(_oe_content_config_import_prepare($file_storage->read('core.entity_form_display.oe_author.oe_corporate_body.default')))->save();
-
-  // Create field storage for nodes.
-  $field_storage_config->create(_oe_content_config_import_prepare($file_storage->read('field.storage.node.oe_authors')))->save();
-
-  // According to the fact that right now we can not control the order of
-  // post_update hook implementation executions, we have to do the handling of
-  // currently enabled content types in the main oe_content module.
-  $content_types = $entity_type_manager->getStorage('node_type')->loadMultiple([
-    'oe_event',
-    'oe_news',
-    'oe_page',
-    'oe_policy',
-    'oe_publication',
-  ]);
-  foreach (array_keys($content_types) as $content_type) {
-    $field_config->create(_oe_content_config_import_prepare($file_storage->read('field.field.node.' . $content_type . '.oe_authors')))->save();
-    $form_display = $file_storage->read('core.entity_form_display.node.' . $content_type . '.default');
-    $entity = $entity_type_manager->getStorage('entity_form_display')->load($form_display['id']);
-    $entity_type_manager->getStorage('entity_form_display')->updateFromStorageRecord($entity, _oe_content_config_import_prepare($form_display))->save();
   }
 }
