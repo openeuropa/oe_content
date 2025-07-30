d1e53fac76a2fb0100282d6ff1384469edb8962c
PaoloScuttiWZ <paolo.scutti@weblitz.it>
2025-07-30 10:48:21 +0200

    Fix #619: Error in _post_update file when updating oe_content_module

--- modules/oe_content_social_media_links_field/oe_content_social_media_links_field.post_update.php
+++ modules/oe_content_social_media_links_field/oe_content_social_media_links_field.post_update.php
@@ -27,6 +27,9 @@ function oe_content_social_media_links_field_post_update_00001(): void {
  */
 function oe_content_social_media_links_field_post_update_00002() {
   $field_storage = \Drupal::entityTypeManager()->getStorage('field_storage_config')->load('node.oe_social_media_links');
+  if (!$field_storage) {
+    return;
+  }
   $settings = $field_storage->get('settings');
   if (!isset($settings['allowed_values']['twitter'])) {
     return 'The field storage does not contain the twitter key.';
